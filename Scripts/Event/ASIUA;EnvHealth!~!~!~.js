37d22b763f0274fed020d981c1430c5ca90d7503
emse30 <bleblanc@hhcorp.org>
2017-07-06 14:42:17 -0400

    Update ASIUA;EnvHealth!~!~!~.js
    Added debugging

--- Scripts/Event/ASIUA;EnvHealth!~!~!~.js
+++ Scripts/Event/ASIUA;EnvHealth!~!~!~.js
@@ -8,6 +8,7 @@
 if (matches(appTypeArray[1],'EHSM','HHECMSC','HOUSING')) { //&& currentUserID == "RVOLLER") { //commented to test as CHAAS
 	//var newUserID = lookup("Census - Housing EHS",AInfo['ParcelAttribute.CensusTract']);
 	var newUserID = lookup("Census - Housing EHS",AInfo['GENERAL.Census Tract']);
+	logDebug("New User ID is: "+newUserID);
 	if (checkInspectionResult("Initial Inspection", "Scheduled") == true) { inspNum=getScheduledInspId("Initial Inspection"); }
 	if (checkInspectionResult("Reinspection", "Scheduled") == true) { inspNum=getScheduledInspId("Reinspection"); }
 	editAppSpecific("GENERAL.Assigned To", newUserID);
