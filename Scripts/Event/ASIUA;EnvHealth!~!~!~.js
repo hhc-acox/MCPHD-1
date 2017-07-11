4bfd1c856dc052999b32cce2462abf81c3cfaaef
emse30 <bleblanc@hhcorp.org>
2017-07-11 14:18:45 -0400

    Update ASIUA;EnvHealth!~!~!~.js
    added logDebug statements to troubleshoot

--- Scripts/Event/ASIUA;EnvHealth!~!~!~.js
+++ Scripts/Event/ASIUA;EnvHealth!~!~!~.js
@@ -7,7 +7,9 @@
 
 if (matches(appTypeArray[1],'EHSM','HHECMSC','HOUSING')) { //&& currentUserID == "RVOLLER") { //commented to test as CHAAS
 	//var newUserID = lookup("Census - Housing EHS",AInfo['ParcelAttribute.CensusTract']);
+	logDebug("appTypeArray1 is: " +appTypeArray[1]);
 	var newUserID = lookup("Census - Housing EHS",AInfo['Census Tract']);
+	logDebug("Census Tract is: " +AInfo['Census Tract']);
 	logDebug("New User ID is: "+newUserID);
 	if (checkInspectionResult("Initial Inspection", "Scheduled") == true) { inspNum=getScheduledInspId("Initial Inspection"); }
 	if (checkInspectionResult("Reinspection", "Scheduled") == true) { inspNum=getScheduledInspId("Reinspection"); }
