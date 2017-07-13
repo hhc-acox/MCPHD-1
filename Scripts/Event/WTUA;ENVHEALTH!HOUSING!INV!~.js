a0811a088e5b5fc17eb7980c26fead5f93c1f6e2
emse30 <bleblanc@hhcorp.org>
2017-07-13 16:46:38 -0400

    Update WTUA;ENVHEALTH!HOUSING!INV!~.js
    Updated to reflect MCPHD environment

--- Scripts/Event/WTUA;ENVHEALTH!HOUSING!INV!~.js
+++ Scripts/Event/WTUA;ENVHEALTH!HOUSING!INV!~.js
@@ -1,8 +1,8 @@
 // WTUA;ENVHEALTH!HOUSING!INV!~
-if (wfTask == 'Inspection' && wfStatus == 'Reinspect' && AInfo['GENERAL.Mosquito Control'] != 'Yes' && AInfo['Reinspection Date'] != null && AInfo['Reinspection Date'] != '') {
-	scheduleInspectDate('Reinspection',AInfo['Reinspection Date'],AInfo['GENERAL.Assigned To']);
+if (wfTask == 'Inspection' && wfStatus == 'Reinspect' && AInfo['Mosquito Control'] != 'Yes' && AInfo['Reinspection Date'] != null && AInfo['Reinspection Date'] != '') {
+	scheduleInspectDate('Reinspection',AInfo['Reinspection Date'],AInfo['Assigned To']);
 	}
 
-if (wfTask == 'Inspection' && wfStatus == 'Reinspect' && AInfo['GENERAL.Mosquito Control'] == 'Yes' && AInfo['Reinspection Date'] != null && AInfo['Reinspection Date'] != '') {
+if (wfTask == 'Inspection' && wfStatus == 'Reinspect' && AInfo['Mosquito Control'] == 'Yes' && AInfo['Reinspection Date'] != null && AInfo['Reinspection Date'] != '') {
 	scheduleInspectDate('Reinspection',AInfo['Reinspection Date'],'LLOBDELL');
 	}
