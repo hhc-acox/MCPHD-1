9d384e0237acbc55d4d111768e2cdf2dcdb5f8d6
emse30 <bleblanc@hhcorp.org>
2017-07-13 15:22:45 -0400

    Update ASIUA;ENVHEALTH!HOUSING!HSG!~.js
    removing GENERAL. from AInfo

--- Scripts/Event/ASIUA;ENVHEALTH!HOUSING!HSG!~.js
+++ Scripts/Event/ASIUA;ENVHEALTH!HOUSING!HSG!~.js
@@ -1,6 +1,6 @@
 // ASIUA;ENVHEALTH!HOUSING!HSG!~
 HEHS = 'RVOLLER';
-if (AInfo['GENERAL.Bed Bugs'] == 'Yes' && AInfo['GENERAL.Bed Bugs Referred'] == null) {
+if (AInfo['Bed Bugs'] == 'Yes' && AInfo['Bed Bugs Referred'] == null) {
 	
 //replaced branch(ES_BBE_CREATE_CHILD_CASE)
 ES_BBE_CREATE_CHILD_CASE();
