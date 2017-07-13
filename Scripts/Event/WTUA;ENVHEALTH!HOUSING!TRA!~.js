231c38fff79235bbe5f0e03f3062bda4d27095a7
emse30 <bleblanc@hhcorp.org>
2017-07-13 16:50:05 -0400

    Update WTUA;ENVHEALTH!HOUSING!TRA!~.js
    Updated to reflect MCPHD environment

--- Scripts/Event/WTUA;ENVHEALTH!HOUSING!TRA!~.js
+++ Scripts/Event/WTUA;ENVHEALTH!HOUSING!TRA!~.js
@@ -1,11 +1,11 @@
 // WTUA;ENVHEALTH!HOUSING!TRA!~
 var myEC = true;
 if (wfTask == 'Initial Processing' && matches(wfStatus,'Complete Notice of Violation','Complete Emergency NOV') && AInfo['Reinspection Date (in 14 days if blank)'] != null && AInfo['Reinspection Date (in 14 days if blank)'] != '') {
-	scheduleInspectDate('Reinspection',AInfo['Reinspection Date (in 14 days if blank)'],AInfo['GENERAL.Assigned To']);
+	scheduleInspectDate('Reinspection',AInfo['Reinspection Date (in 14 days if blank)'],AInfo['Assigned To']);
 	}
 
 if (wfTask == 'Initial Processing' && matches(wfStatus,'Complete Notice of Violation','Complete Emergency NOV') && (AInfo['Reinspection Date (in 14 days if blank)'] == null || AInfo['Reinspection Date (in 14 days if blank)'] == '')) {
-	scheduleInspectDate('Reinspection',nextWorkDay(dateAdd(null,13)),AInfo['GENERAL.Assigned To']), editTaskSpecific('Initial Processing','Reinspection Date (in 14 days if blank)',nextWorkDay(dateAdd(null,13)));
+	scheduleInspectDate('Reinspection',nextWorkDay(dateAdd(null,13)),AInfo['Assigned To']), editTaskSpecific('Initial Processing','Reinspection Date (in 14 days if blank)',nextWorkDay(dateAdd(null,13)));
 	}
 
 if (wfTask == 'Initial Processing' && wfStatus == 'Notice of Violation' && (AInfo['Reinspection Date (in 14 days if blank)'] == null || AInfo['Reinspection Date (in 14 days if blank)'] == '')) {
@@ -17,23 +17,23 @@ if (wfTask == 'Initial Processing' && wfStatus == 'Emergency Notice of Violation
 	}
 
 if (wfTask == 'Reinspection' && wfStatus == 'Reinspection' && AInfo['Reinspect On']!= null) {
-	scheduleInspectDate('Reinspection',AInfo['Reinspect On'],AInfo['GENERAL.Assigned To']);
+	scheduleInspectDate('Reinspection',AInfo['Reinspect On'],AInfo['Assigned To']);
 	}
 
 if (wfTask == 'Additional Processing' && (wfStatus == 'Reinspection' || wfStatus == 'Complete Reinspection Letter' || wfStatus == 'Complete Ticket' || wfStatus == 'Complete Add Vio Reinspection Ltr') && AInfo['Schedule Reinspection On'] != null && AInfo['Schedule Reinspection On'] != '') {
-	scheduleInspectDate('Reinspection',AInfo['Schedule Reinspection On'],AInfo['GENERAL.Assigned To']);
+	scheduleInspectDate('Reinspection',AInfo['Schedule Reinspection On'],AInfo['Assigned To']);
 	}
 
 if (wfTask == 'Additional Processing' && (wfStatus == 'Reinspection' || wfStatus == 'Complete Reinspection Letter' || wfStatus == 'Complete Add Vio Reinspection Ltr') && (AInfo['Schedule Reinspection On'] == null || AInfo['Schedule Reinspection On'] == '')) {
-	scheduleInspectDate('Reinspection',nextWorkDay(dateAdd(null,13)),AInfo['GENERAL.Assigned To']);
+	scheduleInspectDate('Reinspection',nextWorkDay(dateAdd(null,13)),AInfo['Assigned To']);
 	editTaskSpecific('Additional Processing','Schedule Reinspection On',nextWorkDay(dateAdd(null,13)));
 	}
 
-if (wfTask == 'Additional Processing' && AInfo['GENERAL.Owner Occupied'] == 'Yes') {
+if (wfTask == 'Additional Processing' && AInfo['Owner Occupied'] == 'Yes') {
 	cleanType = 'Occupied';
 	}
 
-if (wfTask == 'Additional Processing' && AInfo['GENERAL.Owner Occupied'] == 'No') {
+if (wfTask == 'Additional Processing' && AInfo['Owner Occupied'] == 'No') {
 	cleanType = 'Not Occupied';
 	}
 
@@ -61,7 +61,7 @@ if (!feeExists(tFee,'INVOICED')) {
 //end replaced branch: CMN;HHC!TRA!~!~AddTicketFee
 	activateTask('Ticket');
 	editTaskDueDate('Ticket',AInfo['Ticket Due Date (in 14 days if blank)']);
-	assignTask('Ticket',AInfo['GENERAL.Assigned To']);
+	assignTask('Ticket',AInfo['Assigned To']);
 	}
 
 if (wfTask == 'Additional Processing' && wfStatus == 'Ticket' && (AInfo['Ticket Due Date (in 14 days if blank)'] == null || AInfo['Ticket Due Date (in 14 days if blank)'] == '')) {
@@ -78,16 +78,16 @@ if (!feeExists(tFee,'INVOICED')) {
 //end replaced branch: CMN;HHC!TRA!~!~AddTicketFee
 	activateTask('Ticket');
 	editTaskDueDate('Ticket',nextWorkDay(dateAdd(null,13)));
-	assignTask('Ticket',AInfo['GENERAL.Assigned To']);
+	assignTask('Ticket',AInfo['Assigned To']);
 	editTaskSpecific('Additional Processing','Ticket Due Date (in 14 days if blank)',nextWorkDay(dateAdd(null,13)));
 	}
 
 if (wfTask == 'Final Processing' && (wfStatus == 'Permanent Injunction') && AInfo['Schedule PI Inspection On'] != null && AInfo['Schedule PI Inspection On'] != '') {
-	scheduleInspectDate('Reinspection',AInfo['Schedule PI Inspection On'],AInfo['GENERAL.Assigned To']);
+	scheduleInspectDate('Reinspection',AInfo['Schedule PI Inspection On'],AInfo['Assigned To']);
 	}
 
 if (wfTask == 'Final Processing' && (wfStatus == 'Permanent Injunction') && (AInfo['Schedule PI Inspection On'] == null || AInfo['Schedule PI Inspection On'] == '')) {
-	scheduleInspectDate('Reinspection',nextWorkDay(dateAdd(null,179)),AInfo['GENERAL.Assigned To']);
+	scheduleInspectDate('Reinspection',nextWorkDay(dateAdd(null,179)),AInfo['Assigned To']);
 	editTaskSpecific('Final Processing','Schedule PI Inspection On',nextWorkDay(dateAdd(null,179)));
 	}
 
@@ -96,7 +96,7 @@ if (wfTask == 'Recurring Inspection' && wfStatus == 'Reinspect' && AInfo['Schedu
 	}
 
 if (wfTask == 'Additional Processing' && matches(wfStatus,'Complete Reinspection Letter','Complete Ticket','Complete Add Vio Reinspection Ltr')) {
-	assignTask('Additional Processing',AInfo['GENERAL.Assigned To']);
+	assignTask('Additional Processing',AInfo['Assigned To']);
 	}
 
 if (wfTask == 'EC Clean Occupied' && matches(wfStatus,'Complete Billing Letter') && capStatus == 'Final Processing') {
