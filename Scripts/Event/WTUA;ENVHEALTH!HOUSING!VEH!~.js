b5f885d62b75259fc2b071165736928210d4b7df
emse30 <bleblanc@hhcorp.org>
2017-07-13 16:51:44 -0400

    Update WTUA;ENVHEALTH!HOUSING!VEH!~.js
    Updated to reflect MCPHD environment

--- Scripts/Event/WTUA;ENVHEALTH!HOUSING!VEH!~.js
+++ Scripts/Event/WTUA;ENVHEALTH!HOUSING!VEH!~.js
@@ -1,10 +1,10 @@
 // WTUA;ENVHEALTH!HOUSING!VEH!~
 if (wfTask == 'Initial Processing' && (wfStatus == 'Complete Notice of Violation') && AInfo['Reinspection Date (in 21 days if blank)'] != null && AInfo['Reinspection Date (in 21 days if blank)'] != '') {
-	scheduleInspectDate('Reinspection',AInfo['Reinspection Date (in 21 days if blank)'],AInfo['GENERAL.Assigned To']);
+	scheduleInspectDate('Reinspection',AInfo['Reinspection Date (in 21 days if blank)'],AInfo['Assigned To']);
 	}
 
 if (wfTask == 'Initial Processing' && (wfStatus == 'Complete Notice of Violation') && (AInfo['Reinspection Date (in 21 days if blank)'] == null ||AInfo['Reinspection Date (in 21 days if blank)'] == '')) {
-	scheduleInspectDate('Reinspection',nextWorkDay(dateAdd(null,20)),AInfo['GENERAL.Assigned To']);
+	scheduleInspectDate('Reinspection',nextWorkDay(dateAdd(null,20)),AInfo['Assigned To']);
 	editTaskSpecific('Initial Processing','Reinspection Date (in 21 days if blank)',nextWorkDay(dateAdd(null,20)));
 	}
 
@@ -24,11 +24,11 @@ if (wfTask == 'Additional Processing' && (wfStatus == 'Complete Cntr Referral 18
 	}
 
 if (wfTask == 'Towing Inspection' && wfStatus == 'Reinspection') {
-	scheduleInspectDate('Reinspection',AInfo['Reinspect on'],AInfo['GENERAL.Assigned To']);
+	scheduleInspectDate('Reinspection',AInfo['Reinspect on'],AInfo['Assigned To']);
 	}
 
 if (wfTask == 'Reinspection' && wfStatus == 'Reinspection') {
-	scheduleInspectDate('Reinspection',AInfo['Schedule Reinspection On'],AInfo['GENERAL.Assigned To']);
+	scheduleInspectDate('Reinspection',AInfo['Schedule Reinspection On'],AInfo['Assigned To']);
 	}
 
 if (wfTask == 'Initial Processing' && wfStatus == 'Contractor Referral 48 Hours') {
