e49a770aff5f94cdd977fba98edf7ee7417b12a5
emse30 <bleblanc@hhcorp.org>
2017-07-31 13:44:35 -0400

    Clarify filename and naming convention

--- Scripts/Event/readme2.js
+++ Scripts/Event/readme2.js
@@ -1,13 +1,13 @@
 /*
 
-Event scripts go here.   Filename convention is:
+Event scripts go here.   Filename must be UPPERCASE; Filename convention is:
 
 	AAAA;B!C!D!E.js
 
 where:
 
 	AAAA 	= Event Prefix (see EMSE_VARIABLE_BRANCH_PREFIX standard choice, examples are WTUA, CRCA, ASA)
-	B		= Module (e.g., Licenses)
+	B		= Module (e.g., LICENSES)
 	C		= Application Type, or tilde (~) for wildcard
 	D		= Sub-type, or tilde (~) for wildcard
 	E		= Category, or tilde (~) for wildcard
