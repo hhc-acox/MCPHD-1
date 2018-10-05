a0cee6a36ea38a647e078c9519f20f9571b07a22
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-05 10:08:54 -0600

    moved script to accommodate pmt portlet

--- Scripts/Event/PAA;ENVHEALTH!FOOD!~!APPLICATION.js
+++ Scripts/Event/PAA;ENVHEALTH!FOOD!~!APPLICATION.js
@@ -1,5 +1,4 @@
 //lwacht: 031018: #229: Auto Schedule Initial Inspection
-/* moving to PAA
 try{
 	if(balanceDue==0){
 		 scheduleInspectDate("Initial", nextWorkDay(dateAdd(null,14)));
@@ -8,5 +7,4 @@ try{
 	logDebug("A JavaScript Error occurred: PRA:EnvHealth/Food/* /Application: " + err.message);
 	logDebug(err.stack)
 }
-*/
 //lwacht: 031018: #229: end
