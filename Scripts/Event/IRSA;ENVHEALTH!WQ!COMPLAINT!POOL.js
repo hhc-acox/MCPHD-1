04710d1c56eb2c9e2a40502f146c091a8ac6a8e8
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-08 11:20:57 -0600

    updated logic

--- Scripts/Event/IRSA;ENVHEALTH!WQ!COMPLAINT!POOL.js
+++ Scripts/Event/IRSA;ENVHEALTH!WQ!COMPLAINT!POOL.js
@@ -1,8 +1,15 @@
 //lwacht: 181008: #2: Keeping applicant informed
 try{
 	if(inspResult.indexOf("Violation")>-1 && inspResult!="No Violations Found"){
-		emailNotifContact("TBD", false, "", "Responsible Party", true, "no_reply@accela.com");
-		emailNotifContact("TBD", false, "", "Management Company", true, "no_reply@accela.com");
+		var arrCapIds = getRelatedCapsByParcel("EnvHealth/WQ/Pool/License");
+		for(cap in arrCapIds){
+			var thisCapId = arrCapIds[cap];
+			var currCap = capId;
+			capId = thisCapId.getCapID();
+			emailNotifContact("TBD", false, "", "Responsible Party", true, "no_reply@accela.com");
+			emailNotifContact("TBD", false, "", "Management Company", true, "no_reply@accela.com");
+			capId = currCap;
+		}
 	}
 }catch(err){
 	logDebug("A JavaScript Error occurred: IRSA:EnvHealth/WQ/Complaint/Pool: " + err.message);
