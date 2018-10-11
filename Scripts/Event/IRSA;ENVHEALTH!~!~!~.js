96018cff22895222c870356f4882fde26959fda2
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-11 11:47:16 -0600

    updating logic

--- Scripts/Event/IRSA;ENVHEALTH!~!~!~.js
+++ Scripts/Event/IRSA;ENVHEALTH!~!~!~.js
@@ -1,21 +1,48 @@
-// IRSA;ENVHEALTH!~!~!~ 
-var currDateFix;
-var inspDate;
-var one_day=1000*60*60*24;
-//lwacht: 181011: resolving issues impacting the scripting i'm doing
+showDebug = true; 
+showMessage = true; 
+var riskLevel = AInfo["Risk"];
+//lwacht: 021018: resolving issues impacting the scripting i'm doing
 inspObj = aa.inspection.getInspection(capId,inspId).getOutput();  
-inspDate = inspObj.getInspectionDate().getMonth() + '/' + inspObj.getInspectionDate().getDayOfMonth() + '/' + inspObj.getInspectionDate().getYear();
-var currDate=new Date(dateAdd(null,0));
-var futureDate=new Date(dateAdd(null,365));
-currDateFix=Date.parse(currDate);
-var fixinspDate = Date.parse(inspDate);
-var fixfutureDate = Date.parse(futureDate);
-if (((currDateFix - fixinspDate) / one_day > 90)) {
-	showMessage = true;
-	message = 'The resulted inspection date cannot be more than 90 days in the past.';
-	}
+var inspDate =  inspObj.getInspectionDate().getMonth() + "/" + inspObj.getInspectionDate().getDayOfMonth() + "/" + inspObj.getInspectionDate().getYear();
+logDebug("Inspection Date= " + inspDate);
 
-if (fixfutureDate<fixinspDate) {
-	showMessage = true;
-	message = 'The resulted inspection date cannot be more than a year in the future.';
-	}
+if(inspType == "Pre-Operational Inspection" && inspResult == "Meets Standards"){
+    branchTask("Inspection","Inspection Passed","Updated by Inspection Result","Note");
+} 
+if(inspType == "Pre-Operational Inspection" && inspResult == "Fails to Meet Standards"){
+    message="All Inspections must meets standards before completing the Inspection Workflow Task.<br/><br/>";
+    scheduleInspection("Follow-up Inspection",0,currentUserID);
+    debug=message;
+} 
+if(inspType == "Follow-up Inspection" && inspResult == "Meets Standards"){
+    branchTask("Inspection","Inspection Passed","Updated by Inspection Result","Note");
+} 
+if(inspType == "Follow-up Inspection" && inspResult == "Fails to Meet Standards"){
+    message="All Inspections must meet standards before completing the Inspection Workflow Task.<br/><br/>";
+    debug=message;
+} 
+if(riskLevel!=null && riskLevel==1 && inspType == "Routine Inspection" && inspResult == "Meets Standards"){
+    scheduleInspection("Routine Inspection", Math.round(( new Date(dateAddMonths(inspDate, 12))- new Date())/(1000*60*60*24) + 1), currentUserID);
+} 
+if(riskLevel!=null && riskLevel==2 && inspType == "Routine Inspection" && inspResult == "Meets Standards"){
+    scheduleInspection("Routine Inspection", Math.round(( new Date(dateAddMonths(inspDate, 6))- new Date())/(1000*60*60*24) + 1), currentUserID);
+} 
+if(riskLevel!=null && riskLevel==3 && inspType == "Routine Inspection" && inspResult == "Meets Standards"){
+    scheduleInspection("Routine Inspection", Math.round(( new Date(dateAddMonths(inspDate, 4))- new Date())/(1000*60*60*24) + 1), currentUserID);
+} 
+if(riskLevel!=null && riskLevel==4 && inspType == "Routine Inspection" && inspResult == "Meets Standards"){
+    scheduleInspection("Routine Inspection", Math.round(( new Date(dateAddMonths(inspDate, 3))- new Date())/(1000*60*60*24) + 1), currentUserID);
+} 
+
+if(riskLevel!=null && riskLevel==1 && inspType == "Routine Inspection" && inspResult == "Fails to Meet Standards"){
+    scheduleInspection("Routine Inspection", Math.round(( new Date(dateAddMonths(inspDate, 12))- new Date())/(1000*60*60*24) + 1), currentUserID);
+} 
+if(riskLevel!=null && riskLevel==2 && inspType == "Routine Inspection" && inspResult == "Fails to Meet Standards"){
+    scheduleInspection("Routine Inspection", Math.round(( new Date(dateAddMonths(inspDate, 6))- new Date())/(1000*60*60*24) + 1), currentUserID);
+} 
+if(riskLevel!=null && riskLevel==3 && inspType == "Routine Inspection" && inspResult == "Fails to Meet Standards"){
+    scheduleInspection("Routine Inspection", Math.round(( new Date(dateAddMonths(inspDate, 4))- new Date())/(1000*60*60*24) + 1), currentUserID);
+} 
+if(riskLevel!=null && riskLevel==4 && inspType == "Routine Inspection" && inspResult == "Fails to Meet Standards"){
+    scheduleInspection("Routine Inspection", Math.round(( new Date(dateAddMonths(inspDate, 3))- new Date())/(1000*60*60*24) + 1), currentUserID);
+}
