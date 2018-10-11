97806e595880c76010e5010fdc12302e7eaca425
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-11 12:31:26 -0600

    reschedule inspection if site is breeding

---
+++ Scripts/Event/IRSA;ENVHEALTH!VC!~!NA.js
@@ -0,0 +1,16 @@
+//lwacht: 181011: reschedule inspection if site is breeding
+try{
+	if(matches(appTypeArray[2],"LarvicideSite","MonitorSite")){
+		if(inspResult!="Unable to Inspect"){
+			var siteBreeding = getGuidesheetASIValue(inspId,"VC_Larvicide","SITE INFORMATION","VC_LVCCKLST","LARVICIDE","Is Site Breeding");
+			logDebug("siteBreeding: " + siteBreeding);
+			if(siteBreeding=="Yes"){
+				scheduleInspectDate("Recheck",dateAdd(null,14));
+			}
+		}
+	}
+}catch(err){
+	logDebug("A JavaScript Error occurred: IRSA:EnvHealth/VC/*/NA: " + err.message);
+	logDebug(err.stack)
+}
+//lwacht: 181011: end
