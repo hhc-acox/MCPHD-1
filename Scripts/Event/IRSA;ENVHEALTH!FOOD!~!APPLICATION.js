02d504a4e06b457c2a65cba033f45be33eb07e79
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-15 13:12:24 -0600

    reconciling logic

--- Scripts/Event/IRSA;ENVHEALTH!FOOD!~!APPLICATION.js
+++ Scripts/Event/IRSA;ENVHEALTH!FOOD!~!APPLICATION.js
@@ -1,7 +1,11 @@
 //lwacht: 181015: #49/#37: Auto Scheduling of Inspectiona
 try{
-	if(inspResult.indexOf("Violation")>-1 && matches(inspType,"Routine", "Rechecks") && !getScheduledInspId("Rechecks") && parseInt(AInfo["Risk"])>0){
-		scheduleInspect(capId, "Rechecks", parseInt(AInfo["Risk"]));
+	if(matches(inspType,"Routine", "Rechecks") && !getScheduledInspId("Rechecks") && parseInt(AInfo["Risk"])>0){
+		if(inspResult.indexOf("Violation")>-1){
+			scheduleInspect(capId, "Rechecks", parseInt(AInfo["Risk"]));
+		}else{
+			scheduleInspect(capId, "Routine", parseInt(AInfo["Risk"]));
+		}
 	}else{
 		if(matches(AInfo["Risk"], "",null,"undefined")){
 			showMessage = true; 
