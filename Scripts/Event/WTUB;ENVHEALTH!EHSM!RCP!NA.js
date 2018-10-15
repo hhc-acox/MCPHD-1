d6aaa4455b249d9331ca00b24b7b2de27d8b2e7f
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-15 15:24:45 -0600

    send to court rules

---
+++ Scripts/Event/WTUB;ENVHEALTH!EHSM!RCP!NA.js
@@ -0,0 +1,12 @@
+//lwacht: 181015: #113 Send to Court Rules
+try{
+	if(balanceDue>0 && wfStatus!="Court Order Needed"){
+		cancel=true;
+		showMessage=true;
+		comment("Only 'Court Order Needed' can be selected when there is an outstanding balance.")
+	}
+}catch(err){
+	logDebug("A JavaScript Error occurred: WTUB:EnvHealth/EHSM/RCP/NA: " + err.message);
+	logDebug(err.stack)
+}
+//lwacht: 181015: #113: end
