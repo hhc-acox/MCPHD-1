539fce55c3acf739c248878edf95bffdf0785f82
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-15 15:31:42 -0600

    added task name

--- Scripts/Event/WTUB;ENVHEALTH!EHSM!RCP!NA.js
+++ Scripts/Event/WTUB;ENVHEALTH!EHSM!RCP!NA.js
@@ -1,6 +1,6 @@
 //lwacht: 181015: #113 Send to Court Rules
 try{
-	if(balanceDue>0 && wfStatus!="Court Order Needed"){
+	if(balanceDue>0 && wfTask=="Additional Processing" && wfStatus!="Court Order Needed"){
 		cancel=true;
 		showMessage=true;
 		comment("Only 'Court Order Needed' can be selected when there is an outstanding balance.")
