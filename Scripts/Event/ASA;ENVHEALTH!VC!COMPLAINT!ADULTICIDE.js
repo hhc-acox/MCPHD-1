06a70d1dce6a9ed7d542dfcc83270a29364b2a10
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-16 07:54:40 -0600

    #64: schedule adulticide inspection

---
+++ Scripts/Event/ASA;ENVHEALTH!VC!COMPLAINT!ADULTICIDE.js
@@ -0,0 +1,15 @@
+//lwacht: 181016: #64: Adult Spraying
+try{
+	var startDate = new Date();
+	var hh = startDate.getHours().toString();
+	var mi = startDate.getMinutes().toString();
+	if(hh<17){
+		scheduleInspect(capId, "Adulticide Inspection", 0);
+	}else{
+		scheduleInspect(capId, "Adulticide Inspection", 1);
+	}
+}catch(err){
+	logDebug("A JavaScript Error occurred: ASA:EnvHealth/VC/Complaint/Adulticide: " + err.message);
+	logDebug(err.stack)
+}
+//lwacht: 181016: #64: end
