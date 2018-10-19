ee5518ca07b21dac69230b409b7a3a7474bc6eb2
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-19 09:02:13 -0600

    #123

---
+++ Scripts/Event/WTUA;ENVHEALTH!HOUSING!~!~.js
@@ -0,0 +1,18 @@
+//lwacht: 181019: #123: New Status for Injunction
+try{
+	if(wfStatus=="Permanent Injunction"){
+		if(parentCapId){
+			updateAppStatus("Permanent Injunction","Updated via WTUA:EnvHealth/Housing/*/*", parentCapId);
+			var currCap = capId;
+			capId = parentCapId;
+			if(!isScheduled("Reinspection"){
+				scheduleInspect(parentCapId,"Reinspection",180);
+			}
+			capId= currCap;
+		}
+	}
+}catch(err){
+	logDebug("A JavaScript Error occurred: WTUA:EnvHealth/Housing/*/*: " + err.message);
+	logDebug(err.stack)
+}
+//lwacht: 181019: #123: end
