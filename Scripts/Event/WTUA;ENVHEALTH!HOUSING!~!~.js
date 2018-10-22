8f81dcf06505d6e9b9e3e0917eb25c46559ea181
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-22 10:00:14 -0600

    #123 permanent injunction

--- Scripts/Event/WTUA;ENVHEALTH!HOUSING!~!~.js
+++ Scripts/Event/WTUA;ENVHEALTH!HOUSING!~!~.js
@@ -1,15 +1,27 @@
 //lwacht: 181019: #123: New Status for Injunction
 try{
 	if(wfStatus=="Permanent Injunction"){
+		var parCapId = false;
 		if(parentCapId){
-			updateAppStatus("Permanent Injunction","Updated via WTUA:EnvHealth/Housing/*/*", parentCapId);
-			var currCap = capId;
-			capId = parentCapId;
-			if(!isScheduled("Reinspection")){
-				scheduleInspect(parentCapId,"Reinspection",180);
-			}
-			capId= currCap;
+			parCapId = parentCapId;
+		}else{
+			var parAltId = AInfo["Parent Case"];
+			parCapId = getApplication(parAltId);
 		}
+		if(parCapId){
+			updateAppStatus("Permanent Injunction","Updated via WTUA:EnvHealth/Housing/*/*", parCapId);
+			if(!checkInspectionResult("Reinspection","Scheduled")){
+				var inspUserId = getInspector("Initial Inspection");
+				logDebug("inspUserId: " + inspUserId);
+				if(inspUserId){
+					scheduleInspect(capId,"Reinspection",180,inspUserId);
+				}else{
+					scheduleInspect(capId,"Reinspection",180);
+				}
+			}
+		}else{
+			logDebug("No parent record found.  No reinspection scheduled.");
+		}			
 	}
 }catch(err){
 	logDebug("A JavaScript Error occurred: WTUA:EnvHealth/Housing/*/*: " + err.message);
