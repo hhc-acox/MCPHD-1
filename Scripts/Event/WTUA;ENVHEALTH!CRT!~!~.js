3c9d7a2930ec602ad95563e7b69eff54eda8c3e0
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-30 08:22:27 -0600

    124 removing injunctions

--- Scripts/Event/WTUA;ENVHEALTH!CRT!~!~.js
+++ Scripts/Event/WTUA;ENVHEALTH!CRT!~!~.js
@@ -26,7 +26,7 @@ try{
 		}			
 	}
 }catch(err){
-	logDebug("A JavaScript Error occurred: WTUA:EnvHealth/Housing/CRT/*: permanent Injunction: " + err.message);
+	logDebug("A JavaScript Error occurred: WTUA:EnvHealth/Housing/CRT/*: Permanent Injunction: " + err.message);
 	logDebug(err.stack)
 }
 //lwacht: 181019: #123: end
@@ -37,7 +37,32 @@ try{
 		updateFee("H_T100", "H_TRA", "FINAL", 1, "Y", "Y");
 	}
 }catch(err){
-	logDebug("A JavaScript Error occurred: WTUA:EnvHealth/Housing/CRT/*: Fee assessments: " + err.message);
+	logDebug("A JavaScript Error occurred: WTUA:EnvHealth/CRT/*/*: Fee assessments: " + err.message);
 	logDebug(err.stack)
 }
 //lwacht: 181022: #143: end
+
+//lwacht: 181030: #124: Efficiencies when closing Injunction
+try{
+	if(matches(wfStatus,"Cause Dismiss","Compliance","Contempt","Dismissed")){
+		var parCapId = false;
+		if(parentCapId){
+			parCapId = parentCapId;
+		}else{
+			var parAltId = AInfo["Parent Case"];
+			parCapId = getApplication(parAltId);
+		}
+		if(parCapId){
+			updateAppStatus("Active","Updated via WTUA:EnvHealth/Housing/*/*", parCapId);
+			if(checkInspectionResult("Reinspection","Scheduled")){
+				resultInspection("Reinspection", "Cancelled", sysDate, "Updated via WTUA:EnvHealth/Housing/*/*") 
+			}
+		}else{
+			logDebug("No parent record found.  No updates made.");
+		}			
+	}
+}catch(err){
+	logDebug("A JavaScript Error occurred: WTUA:EnvHealth/CRT/*/*: Closing Injunction: " + err.message);
+	logDebug(err.stack)
+}
+//lwacht: 181030: #124: end
