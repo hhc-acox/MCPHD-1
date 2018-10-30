b8dcc1140f274809714c390660c6351a665aa559
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-30 08:23:39 -0600

    injunction logic

--- Scripts/Event/WTUA;ENVHEALTH!HOUSING!CRT!~.js
+++ Scripts/Event/WTUA;ENVHEALTH!HOUSING!CRT!~.js
@@ -41,3 +41,28 @@ try{
 	logDebug(err.stack)
 }
 //lwacht: 181022: #143: end
+
+//lwacht: 181030: #124: Efficiencies when closing Injunction
+try{
+	if(matches(wfStatus,"Cause Dismiss","Compliance","Contempt","Dismissed")){
+		var parCapId = false;
+		if(parentCapId){
+			parCapId = parentCapId;
+		}else{
+			var parAltId = AInfo["Parent Case"];
+			parCapId = getApplication(parAltId);
+		}
+		if(parCapId){
+			updateAppStatus("Active","Updated via WTUA:EnvHealth/Housing/*/*", parCapId);
+			if(checkInspectionResult("Reinspection","Scheduled")){
+				resultInspection("Reinspection", "Cancelled", sysDate, "Updated via WTUA:EnvHealth/Housing/*/*") 
+			}
+		}else{
+			logDebug("No parent record found.  No updates made.");
+		}			
+	}
+}catch(err){
+	logDebug("A JavaScript Error occurred: WTUA:EnvHealth/Housing/CRT/*: Closing Injunction: " + err.message);
+	logDebug(err.stack)
+}
+//lwacht: 181030: #124: end
