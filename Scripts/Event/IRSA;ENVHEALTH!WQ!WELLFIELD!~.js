7a99626a85661f6e83ec5ba3b7a9345e1ea7841a
Lynda Wacht <lwacht@users.noreply.github.com>
2018-10-31 16:47:26 -0600

    210 wellfield inspection

---
+++ Scripts/Event/IRSA;ENVHEALTH!WQ!WELLFIELD!~.js
@@ -0,0 +1,39 @@
+//lwacht: 181031 #210: Wellfield Inspections
+try{
+	if(!checkInspectionResult("Recheck","Scheduled") && !matches(AInfo["Inspection Cycle"], "","undefined",null) ) {
+		var inspCycle = ""+ AInfo["Inspection Cycle"];
+		switch(inspCycle){
+			case "1 Year":
+				var sDate = dateAdd(null,364);
+				var schDate = nextWorkDay(sDate);
+				break;
+			case "2 Year":
+				var sDate = dateAddMonths(null,24);
+				var schDate = nextWorkDay(sDate);
+				break;
+			case "3 Months":
+				var sDate = dateAddMonths(null,3);
+				var schDate = nextWorkDay(sDate);
+				break;
+			case "3 Year":
+				var sDate = dateAddMonths(null,36);
+				var schDate = nextWorkDay(sDate);
+				break;
+			case "5 Year":
+				var sDate = dateAddMonths(null,60);
+				var schDate = nextWorkDay(sDate);
+				break;
+			case "6 Months":
+				var sDate = dateAddMonths(null,6);
+				var schDate = nextWorkDay(sDate);
+				break;
+		scheduleInspectDate("Recheck", schDate);
+	}else{
+		if(matches(AInfo["Inspection Cycle"], "","undefined",null)){
+			showMessage=true;
+			comment("Inspection Cycle custom field is blank--no recheck was scheduled.");
+}catch(err){
+	logDebug("A JavaScript Error occurred: IRSA:EnvHealth/WQ/Wellfield/*: " + err.message);
+	logDebug(err.stack)
+}
+//lwacht: 181031: #210: end
