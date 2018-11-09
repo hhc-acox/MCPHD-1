39b30ffb7338dd8fc8bdbc4330f2aa6a3e83a887
Lynda Wacht <lwacht@users.noreply.github.com>
2018-11-09 09:56:49 -0700

    autoassignment of inspector.

--- Scripts/Event/ASA;ENVHEALTH!FOOD!COMPLAINT!~.js
+++ Scripts/Event/ASA;ENVHEALTH!FOOD!COMPLAINT!~.js
@@ -3,6 +3,10 @@ try{
 	if(matches(appTypeArray[3],"Foodborne","Other")){
 		var insDate = dateAdd(null, 1, "Y");
 		scheduleInspectDate("Complaint", 1,insDate);
+		var inspectId = getScheduledInspId("Complaint");
+		if(inspectId){
+			autoAssignInspection(inspectId);
+		}
 	}
 }catch(err){
 	logDebug("A JavaScript Error occurred: ASA:EnvHealth/Food/Complaint/*: " + err.message);
