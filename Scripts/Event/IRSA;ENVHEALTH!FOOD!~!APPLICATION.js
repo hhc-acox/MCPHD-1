2f171be9ec84c1a9ce8903af2dbe8a377fe878c3
Lynda Wacht <lwacht@users.noreply.github.com>
2018-11-09 10:28:50 -0700

    clarifying logic

--- Scripts/Event/IRSA;ENVHEALTH!FOOD!~!APPLICATION.js
+++ Scripts/Event/IRSA;ENVHEALTH!FOOD!~!APPLICATION.js
@@ -1,10 +1,14 @@
-//lwacht: 181015: #49/#37: Auto Scheduling of Inspectiona
+//lwacht: 181015: #49: Auto Scheduling of Inspectiona
 try{
-	if(matches(inspType,"Routine", "Rechecks") && !checkInspectionResult("Rechecks","Scheduled") && parseInt(AInfo["Risk"])>0){
+	if(matches(inspType,"Routine", "Rechecks") && parseInt(AInfo["Risk"])>0){
 		if(inspResult.indexOf("Violation")>-1){
-			scheduleInspect(capId, "Rechecks", parseInt(AInfo["Risk"]));
+			if(!checkInspectionResult("Rechecks","Scheduled")){
+				scheduleInspect(capId, "Rechecks", parseInt(AInfo["Risk"]));
+			}
 		}else{
-			scheduleInspect(capId, "Routine", parseInt(AInfo["Risk"]));
+			if(!checkInspectionResult("Routine","Scheduled")){
+				scheduleInspect(capId, "Routine", parseInt(AInfo["Risk"]));
+			}
 		}
 	}else{
 		if(matches(AInfo["Risk"], "",null,"undefined")){
@@ -16,4 +20,4 @@ try{
 	logDebug("A JavaScript Error occurred: IRSA:EnvHealth/Food/*/Application: " + err.message);
 	logDebug(err.stack)
 }
-//lwacht: 181015: #49: end
+//lwacht: 181015: #49/#37: end
