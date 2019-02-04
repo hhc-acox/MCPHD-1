57ca8ebbe8cbb07516733eb018fa1420c75dc29b
Lynda Wacht <lwacht@users.noreply.github.com>
2019-02-04 11:05:30 -0700

    gis function

--- Scripts/Event/ASA;~!~!~!~.js
+++ Scripts/Event/ASA;~!~!~!~.js
@@ -1,4 +1,13 @@
 
+//copy the gis object onto the record so that all gis-related functions work
+try{
+	copyParcelGisObjects() 
+}catch (err){
+	logDebug("A JavaScript Error occurred: ASA:*/*/*/*: Copy GIS Object: " + err.message);
+	logDebug(err.stack)
+}
+	
+
 //assess fees
 try{
 	//see if any records are set up--module can be specific or "ALL", look for both
