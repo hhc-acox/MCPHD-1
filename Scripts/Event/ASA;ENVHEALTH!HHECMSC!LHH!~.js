03a39905f985279d4ed9d13fbf64d4db19f3f3d7
emse30 <bleblanc@hhcorp.org>
2019-03-28 16:36:33 -0400

    commit by emse tools 2019-03-28 16:36:31

--- Scripts/Event/ASA;ENVHEALTH!HHECMSC!LHH!~.js
+++ Scripts/Event/ASA;ENVHEALTH!HHECMSC!LHH!~.js
@@ -1,10 +1,1 @@
-// ASA;ENVHEALTH!HHECMSC!LHH!~
-// 7.5.17 chaas: this script has no issues :)
-scheduleInspectDate('Initial Lead Inspection',AInfo['GENERAL.Initial Inspection Date'],areaInspector);
-theDate = AInfo['GENERAL.Initial Inspection Date'].substring(6,10) + '-' + AInfo['GENERAL.Initial Inspection Date'].substring(0,2) + '-' + AInfo['GENERAL.Initial Inspection Date'].substring(3,5);
-comment('The new date is ' + theDate);
-resultInspection('Initial Lead Inspection','In Violation',theDate,'Resulted by Script');
-assignCap(areaInspector);
-editAppSpecific('GENERAL.Assigned To',areaInspector);
-editAppSpecific('GENERAL.Previous Assigned To',areaInspector);
-updateAppStatus('In Violation','Initial Status');
+// Enter your script here...
