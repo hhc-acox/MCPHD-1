de9308375cbe3cdc3a9f08ed611904c473e8c69a
Lynda Wacht <lwacht@users.noreply.github.com>
2019-03-28 16:34:19 -0600

    test

--- Scripts/Event/GSUB;ENVHEALTH!WQ!POOL!LICENSE.js
+++ Scripts/Event/GSUB;ENVHEALTH!WQ!POOL!LICENSE.js
@@ -1,6 +1,7 @@
 //lwacht: 190318: require at least one field in the pool status table
 try{
 	if (GuidesheetModel && "POOL TEST RESULTS"== GuidesheetModel.getGuideType().toUpperCase()) {
+		var hasPoolStatus = false;
 		var guidesheetItem = GuidesheetModel.getItems();
 		for(var j=0;j< guidesheetItem.size();j++) {
 			var item = guidesheetItem.get(j);
@@ -11,26 +12,22 @@ try{
 					if(guideItemASIT && "POOL STATUS" == guideItemASIT.getTableName().toUpperCase()){
 						var tableArr = new Array();
 						var columnList = guideItemASIT.getColumnList();
-							for (var k = 0; k < columnList.size() ; k++ ){
-								var column = columnList.get(k);
-								var values = column.getValueMap().values();
-								var iteValues = values.iterator();
-								logDebug("iteValues: " + iteValues.hasNext());
-								while(iteValues.hasNext()){
-									var i = iteValues.next();
-									var zeroBasedRowIndex = i.getRowIndex()-1;
-									if (tableArr[zeroBasedRowIndex] == null) tableArr[zeroBasedRowIndex] = new Array();
-									tableArr[zeroBasedRowIndex][column.getColumnName()] = i.getAttributeValue();
-								}
+						for (var k = 0; k < columnList.size() ; k++ ){
+							var column = columnList.get(k);
+							var values = column.getValueMap().values();
+							var iteValues = values.iterator();
+							if(iteValues.hasNext()){
+								hasPoolStatus = true;
 							}
-						if(!columnList || columnList.length<1 || columnList.length=="undefined"){
-							cancel=true;
-							showMessage=true;
-							comment("At least one row needs to be populated in the Pool Status table.");
 						}
 					}
 				}
 			}
+		}
+		if(!hasPoolStatus){
+			cancel=true;
+			showMessage=true;
+			comment("At least one row needs to be populated in the Pool Status table.");
 		}
 	}
 }catch(err){
