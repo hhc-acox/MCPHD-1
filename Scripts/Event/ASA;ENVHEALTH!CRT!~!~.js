f6a6e27ad3efdd3484c4bfab5ebb03c6b145fe0e
emse30 <bleblanc@hhcorp.org>
2019-05-09 15:55:29 -0400

    commit by emse tools 2019-05-09 15:55:28

--- Scripts/Event/ASA;ENVHEALTH!CRT!~!~.js
+++ Scripts/Event/ASA;ENVHEALTH!CRT!~!~.js
@@ -1,5 +1,5 @@
 // ASA;ENVHEALTH!CRT!~!~ 
 // 7/5/17 The user MSTEINER is not in MCPHD configuration
-assignCap('MSTEINBR');
+//assignCap('MSTEINBR');
 updateAppStatus('Legal Review','Initial status'); //verified app status
 updateWorkDesc('Environmental Court');
