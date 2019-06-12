1b41f2c50434779c6ede28e8cfc21cb8bc696b90
emse30 <bleblanc@hhcorp.org>
2019-06-12 14:00:12 -0400

    commit by emse tools 2019-06-12 14:00:09

--- Scripts/Event/ASIUA;ENVHEALTH!HOUSING!HSG!~.js
+++ Scripts/Event/ASIUA;ENVHEALTH!HOUSING!HSG!~.js
@@ -1,7 +1,1 @@
-// ASIUA;ENVHEALTH!HOUSING!HSG!~
-HEHS = 'RVOLLER';
-if (AInfo['Bed Bugs'] == 'Yes' && AInfo['Bed Bugs Referred'] == null) {
-	
-//replaced branch(ES_BBE_CREATE_CHILD_CASE)
-ES_BBE_CREATE_CHILD_CASE();
-	}
+// Enter your script here...
