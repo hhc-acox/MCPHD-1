3cacb7de67dba263e7ef0b4a727bb3cb80febba8
Richard Voller <rvoller@hhcorp.org>
2019-07-08 14:58:05 -0400

    Update HHC_Custom_Functions.js

--- INCLUDES_CUSTOM/HHC_Custom_Functions.js
+++ INCLUDES_CUSTOM/HHC_Custom_Functions.js
@@ -857,3 +857,18 @@ function convertForAssignedTo(areaInspector){
 		logDebug(err.stack);
 	}
 }
+function tableHasRows(t) {
+	try{
+		eval("if (typeof(" + t + ") != \"undefined\" ) var o = " + t);
+			if (typeof(o) == "object" && o.length > 0) {
+				return true;
+	} 		else {
+				return false;
+	}
+	catch(err)
+		{
+		logDebug("A JavaScript Error occurred: tableHasRows:  " + err.message);
+		logDebug(err.stack);
+		}
+	}
+}
