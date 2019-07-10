825f9fdecfd0d12510a620b3528ea7720c61ed36
Richard Voller <rvoller@hhcorp.org>
2019-07-10 11:23:44 -0400

    Update IRMA;ENVHEALTH!HOUSING!TRA!~.js

--- Scripts/Event/IRMA;ENVHEALTH!HOUSING!TRA!~.js
+++ Scripts/Event/IRMA;ENVHEALTH!HOUSING!TRA!~.js
@@ -1,31 +1,56 @@
 // IRMA;ENVHEALTH!HOUSING!TRA!~
+if (isTaskActive('Initial Processing') && matches(inspResult,'In Compliance')) {
+	closeTask('Initial Processing','In Compliance','Updated by Script');
+	
+HHC_VIOLATIONS_LOOP();
+	}
+
 if (isTaskActive('Reinspection') && matches(inspResult,'In Violation','In Violation - Ticket Issued')) {
 	closeTask('Reinspection','In Violation','Updated by Script');
-	assignTask('Additional Processing',AInfo['Assigned To']);
+	assignTask('Additional Processing',AInfo['Assigned To']);  //assignment not working - investigate
 	}
 
 if (isTaskActive('Reinspection') && matches(inspResult,'In Compliance')) {
 	branchTask('Reinspection','In Compliance','Updated by Script');
-	
-//replaced branch(ES_VIOLATIONS_LOOP)
-ES_VIOLATIONS_LOOP();
+
+HHC_VIOLATIONS_LOOP();
 	}
 
 if (isTaskActive('Reinspection') && matches(inspResult,'Non-Compliance/Case Closed')) {
 	branchTask('Reinspection','Non-Compliance/Case Closed','Updated by Script');
-	
-//replaced branch(ES_VIOLATIONS_LOOP)
-ES_VIOLATIONS_LOOP();
+
+HHC_VIOLATIONS_LOOP();
 	}
 
-if (isTaskActive('Additional Processing') && matches(inspResult,'In Compliance')) {
+if (isTaskActive('Additional Processing') && matches(inspResult,'Non-Compliance/Case Closed') && balanceDue > 0) {
+	branchTask('Additional Processing','Non-Compliance/Case Closed','Updated by Script');
+	deactivateTask('Recurring Inspection');
+
+HHC_VIOLATIONS_LOOP();
+	}
 	
-//replaced branch(ES_VIOLATIONS_LOOP)
-ES_VIOLATIONS_LOOP();
+if (isTaskActive('Additional Processing') && matches(inspResult,'In Compliance') && balanceDue > 0) {
+	branchTask('Additional Processing','In Compliance','Updated by Script');
+	deactivateTask('Recurring Inspection');
+
+HHC_VIOLATIONS_LOOP();
 	}
 
-if (isTaskActive('Additional Processing') && matches(inspResult,'Non-Compliance/Case Closed')) {
+if (isTaskActive('Additional Processing') && matches(inspResult,'In Compliance') && balanceDue == 0) {
+	branchTask('Additional Processing','In Compliance','Updated by Script');
+
+HHC_VIOLATIONS_LOOP();
+	}
+
+if (isTaskActive('Additional Processing') && matches(inspResult,'Non-Compliance/Case Closed') && balanceDue == 0) {
+	branchTask('Additional Processing','Non-Compliance/Case Closed','Updated by Script');
+
+HHC_VIOLATIONS_LOOP();
+	}
 	
-//replaced branch(ES_VIOLATIONS_LOOP)
-ES_VIOLATIONS_LOOP();
+if (isTaskActive('Final Processing') && matches(inspResult,'Non-Compliance/Case Closed','In Compliance')) {
+	closeTask('Final Processing','Finaled','Updated by Script');
+	deactivateTask('Recurring Inspection');
+
+HHC_VIOLATIONS_LOOP();
 	}
