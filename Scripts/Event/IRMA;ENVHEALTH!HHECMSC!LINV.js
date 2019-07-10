bbec475f36aa5ff4406b78bc4913d9e0bb3deba4
Richard Voller <rvoller@hhcorp.org>
2019-07-10 14:00:16 -0400

    Add files via upload

---
+++ Scripts/Event/IRMA;ENVHEALTH!HHECMSC!LINV.js
@@ -0,0 +1,23 @@
+//IRMA:ENVHEALTH/HHECMSC/LINV/!
+if (isTaskActive('Inspection') && matches(inspResult,'Unjustified')) {
+	branchTask('Inspection','Unjustified','Updated by Script');
+	}
+
+if (isTaskActive('Inspection') && matches(inspResult,'Duplicate')) {
+	branchTask('Inspection','Duplicate INV - Case Finaled','Updated by Script');
+	}
+
+if (isTaskActive('Inspection') && matches(inspResult,'Pending Order')) {
+	branchTask('Inspection','Pending Order - Case Finaled','Updated by Script');
+	}
+
+if (isTaskActive('Inspection') && matches(inspResult,'In Violation')) {
+	closeTask('Inspection','In Violation','Updated by Script');
+	editAppSpecific('Resulted in Violation','Yes');
+	}
+
+if (inspType == 'Initial Inspection') {
+	inspIDate = inspObj.getInspectionDate().getMonth() + '/' + inspObj.getInspectionDate().getDayOfMonth() + '/' + inspObj.getInspectionDate().getYear();
+	editAppSpecific('Initial Inspection Date',inspIDate);
+	}
+
---
+++ Scripts/Event/IRSAENVHEALTH!HHECMSC!LINV.js
@@ -0,0 +1,22 @@
+//IRSA:ENVHEALTH/HHECMSC/LINV/*
+if (isTaskActive('Inspection') && matches(inspResult,'Unjustified')) {
+	branchTask('Inspection','Unjustified','Updated by Script');
+	}
+
+if (isTaskActive('Inspection') && matches(inspResult,'Duplicate')) {
+	branchTask('Inspection','Duplicate INV - Case Finaled','Updated by Script');
+	}
+
+if (isTaskActive('Inspection') && matches(inspResult,'Pending Order')) {
+	branchTask('Inspection','Pending Order - Case Finaled','Updated by Script');
+	}
+
+if ((inspType == 'Initial Lead Inspection') && matches(inspResult,'In Violation')) {
+	editAppSpecific('Resulted in Violation','Yes');
+	updateAppStatus("Pending Case Creation");
+	}
+
+if (inspType == 'Initial Lead Inspection') {
+	inspIDate = inspObj.getInspectionDate().getMonth() + '/' + inspObj.getInspectionDate().getDayOfMonth() + '/' + inspObj.getInspectionDate().getYear();
+	editAppSpecific('Initial Inspection Date',inspIDate);
+	}
