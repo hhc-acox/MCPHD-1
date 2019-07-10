4876b078c96237b170fe69aaf3957dbc3dcb6bd7
Richard Voller <rvoller@hhcorp.org>
2019-07-10 12:19:57 -0400

    Add files via upload

---
+++ Scripts/Event/IRSA;ENVHEALTH!HHECMSC!LHH!~.js
@@ -0,0 +1,9 @@
+//IRSA;ENVHEALTH!HHECMSC!LHH!~
+if (isTaskActive('Reinspection') && matches(inspResult,'In Compliance')) {
+	branchTask('Reinspection','In Compliance','Updated by Script');
+	}
+
+if (isTaskActive('Reinspection') && matches(inspResult,'Non-Compliance/Case Closed')) {
+	branchTask('Reinspection','Non-Compliance/Case Closed','Updated by Script');
+	}
+
