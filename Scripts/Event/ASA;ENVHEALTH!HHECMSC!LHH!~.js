479c4ff2122b1e982785a17ac48cf8030e0f7425
Richard Voller <rvoller@hhcorp.org>
2019-07-10 12:00:17 -0400

    Update ASA;ENVHEALTH!HHECMSC!LHH!~.js

--- Scripts/Event/ASA;ENVHEALTH!HHECMSC!LHH!~.js
+++ Scripts/Event/ASA;ENVHEALTH!HHECMSC!LHH!~.js
@@ -1,7 +1,1 @@
-if (isTaskActive('Reinspection') && matches(inspResult,'In Compliance')) {
-	branchTask('Reinspection','In Compliance','Updated by Script');
-	}
 
-if (isTaskActive('Reinspection') && matches(inspResult,'Non-Compliance/Case Closed')) {
-	branchTask('Reinspection','Non-Compliance/Case Closed','Updated by Script');
-	}
