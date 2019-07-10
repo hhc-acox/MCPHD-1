6ad076fe24569637d084904f07845d96153b6db6
Richard Voller <rvoller@hhcorp.org>
2019-07-10 08:44:21 -0400

    Update IRSA;ENVHEALTH!HOUSING!HSG!~.js

--- Scripts/Event/IRSA;ENVHEALTH!HOUSING!HSG!~.js
+++ Scripts/Event/IRSA;ENVHEALTH!HOUSING!HSG!~.js
@@ -2,43 +2,30 @@
 if (isTaskActive('Reinspection') && matches(inspResult,'In Compliance')) {
 	branchTask('Reinspection','In Compliance','Updated by Script');
 	
-//replaced branch(ES_VIOLATIONS_LOOP)
-ES_VIOLATIONS_LOOP();
+HHC_VIOLATIONS_LOOP();
 	}
 
 if (isTaskActive('Reinspection') && matches(inspResult,'Non-Compliance/Case Closed')) {
 	branchTask('Reinspection','Non-Compliance/Case Closed','Updated by Script');
 	
-//replaced branch(ES_VIOLATIONS_LOOP)
-ES_VIOLATIONS_LOOP();
+HHC_VIOLATIONS_LOOP();
 	}
 
 if (isTaskActive('Initial Processing') && matches(inspResult,'In Compliance')) {
 	closeTask('Initial Processing','In Compliance','Updated by Script');
 	
-//replaced branch(ES_VIOLATIONS_LOOP)
-ES_VIOLATIONS_LOOP();
+HHC_VIOLATIONS_LOOP();
 	}
 
 if (isTaskActive('Initial Processing') && matches(inspResult,'Non-Compliance/Case Closed')) {
 	closeTask('Initial Processing','Non-Compliance/Case Closed','Updated by Script');
 	
-//replaced branch(ES_VIOLATIONS_LOOP)
-ES_VIOLATIONS_LOOP();
+HHC_VIOLATIONS_LOOP();
 	}
 
-if (isTaskActive('Final Processing') && matches(inspResult,'Non-Compliance/Case Closed','In Compliance') && balanceDue == 0) {
-	closeTask('Final Processing','Finaled','Updated by Script');
+if (isTaskActive('Final Processing') && matches(inspResult,'Non-Compliance/Case Closed','In Compliance')) {
+	closeTask('Final Processing','Closed','Updated by Script');
 	deactivateTask('Recurring Inspection');
 	
-//replaced branch(ES_VIOLATIONS_LOOP)
-ES_VIOLATIONS_LOOP();
-	}
-
-if (isTaskActive('Final Processing') && matches(inspResult,'Non-Compliance/Case Closed','In Compliance') && balanceDue > 0) {
-	updateTask('Final Processing','Closed/Fees Outstanding','Updated by Script');
-	deactivateTask('Recurring Inspection');
-	
-//replaced branch(ES_VIOLATIONS_LOOP)
-ES_VIOLATIONS_LOOP();
+HHC_VIOLATIONS_LOOP();
 	}
