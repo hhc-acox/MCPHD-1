dbc55cb85f52cc7fb6b539b292a1896fec96fa77
Richard Voller <rvoller@hhcorp.org>
2019-07-10 15:44:27 -0400

    Update IRSA;ENVHEALTH!HOUSING!VEH!~.js

--- Scripts/Event/IRSA;ENVHEALTH!HOUSING!VEH!~.js
+++ Scripts/Event/IRSA;ENVHEALTH!HOUSING!VEH!~.js
@@ -1,7 +1,11 @@
-// IRSA;ENVHEALTH!HOUSING!VEH!~
+//IRSA;ENVHEALTH!HOUSING!VEH!~
+	var areaInspector = '';
+	var censusTract = '';
+		censusTract = AInfo['ParcelAttribute.CensusTract'];
+		areaInspector = lookup('Census - Housing EHS',censusTract);
 if (isTaskActive('Reinspection') && matches(inspResult,'In Violation')) {
 	closeTask('Reinspection','In Violation','Updated by Script');
-	assignTask('Additional Processing',AInfo['Assigned To']);
+	assignTask('Additional Processing',areaInspector);
 	}
 
 if (isTaskActive('Reinspection') && matches(inspResult,'In Compliance','Non-Compliance/Case Closed')) {
@@ -10,7 +14,7 @@ if (isTaskActive('Reinspection') && matches(inspResult,'In Compliance','Non-Comp
 
 if (isTaskActive('Towing Inspection') && matches(inspResult,'In Violation')) {
 	closeTask('Inspection','In Violation','Updated by Script');
-	assignTask('Final Processing',AInfo['Assigned To']);
+	assignTask('Final Processing',areaInspector);
 	}
 
 if (isTaskActive('Towing Inspection') && matches(inspResult,'In Compliance','Non-Compliance/Case Closed')) {
@@ -25,5 +29,4 @@ if (isTaskActive('Additional Processing') && matches(inspResult,'In Compliance')
 if (isTaskActive('Additional Processing') && matches(inspResult,'Non-Compliance/Case Closed')) {
 	closeTask('Additional Processing','Non-Compliance/Case Closed','Updated by Script');
 	deactivateTask('Towing Inspection');
-	
 	}
