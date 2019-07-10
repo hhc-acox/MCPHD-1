1fda8496471945a15b92e93b5e382f56184d869b
Richard Voller <rvoller@hhcorp.org>
2019-07-10 15:06:02 -0400

    Update ASIUA;ENVHEALTH!HOUSING!INV!~.js

--- Scripts/Event/ASIUA;ENVHEALTH!HOUSING!INV!~.js
+++ Scripts/Event/ASIUA;ENVHEALTH!HOUSING!INV!~.js
@@ -1,14 +1,18 @@
 // ASIUA;ENVHEALTH!HOUSING!INV!~
-if (capStatus == 'Pending Case Creation' && AInfo['REQUIRED CASES.VEH'] == 'CHECKED') {
+var areaInspector = '';
+var censusTract = '';
+censusTract = AInfo['ParcelAttribute.CensusTract'];
+areaInspector = lookup('Census - Housing EHS',censusTract); 
+if (capStatus == 'Pending Case Creation' && AInfo['VEH'] == 'CHECKED') {
 
  {
 editAppSpecific('VEH Created',dateAdd(null,0));
 updateAppStatus('Finaled','Child Case Created');
-newChildID = createChild('ENVHEALTH','Housing','VEH','NA','');
+newChildID = createChild('EnvHealth','Housing','VEH','NA','');
 copyAppSpecific(newChildID);
 comment('New child app id = '+ newChildID);
 updateAppStatus('In Violation','Created from INV',newChildID);
-assignCap(AInfo['Assigned To'],newChildID);
+assignCap(areaInspector,newChildID);
 editAppSpecific('INV Case',capIDString,newChildID);
 editAppName(capName,newChildID);
 HHC_GET_ADDRESS_FOR_CHILD();
@@ -29,11 +33,11 @@ if (capStatus == 'Pending Case Creation' && AInfo['HSG'] == 'CHECKED') {
 editAppSpecific('HSG Created',dateAdd(null,0),capId);
 var RFI = AInfo['Reason for Invest'];
 updateAppStatus('Finaled','Child Case Created');
-newChildID = createChild('ENVHEALTH','Housing','HSG','NA','');
+newChildID = createChild('EnvHealth','Housing','HSG','NA','');
 copyAppSpecific(newChildID);
 comment('New child app id = '+ newChildID);
 updateAppStatus('In Violation','Created from INV',newChildID);
-assignCap(AInfo['Assigned To'],newChildID);
+assignCap(areaInspector,newChildID);
 editAppSpecific('INV Case',capIDString,newChildID);
 editAppSpecific('Emergency','N',newChildID);
 editAppSpecific('Ordinance Chapter','10-Residential',newChildID);
@@ -54,11 +58,11 @@ if (capStatus == 'Pending Case Creation' && AInfo['TRA'] == 'CHECKED') {
 editAppSpecific('REQUIRED CASES.TRA Created',dateAdd(null,0),capId);
 var RFI = AInfo['Reason for Invest'];
 updateAppStatus('Finaled','Child Case Created');
-newChildID = createChild('ENVHEALTH','Housing','TRA','NA','');
+newChildID = createChild('EnvHealth','Housing','TRA','NA','');
 copyAppSpecific(newChildID);
 comment('New child app id = '+ newChildID);
 updateAppStatus('In Violation','Created from INV',newChildID);
-assignCap(AInfo['Assigned To'],newChildID);
+assignCap(areaInspector,newChildID);
 editAppSpecific('INV Case',capIDString,newChildID);
 editAppSpecific('Emergency','N',newChildID);
 editAppSpecific('Ordinance Chapter','10-Residential',newChildID);
@@ -67,7 +71,7 @@ copyParcelGisObjects();
 HHC_GET_ADDRESS_FOR_CHILD();
 HHC_CONTACTS_PROCESS();
 
-if (AInfo['INVESTIGATION TYPE.Online Complaint'] == 'CHECKED') {
+if (AInfo['Online Complaint'] == 'CHECKED') {
 	editAppSpecific('Reason for Invest','Online Complaint',newChildID);
 		}
 	}
