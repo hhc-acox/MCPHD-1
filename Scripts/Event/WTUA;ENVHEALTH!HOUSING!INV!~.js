40efa34aef013f165b700743a8991cffabcb8753
Richard Voller <rvoller@hhcorp.org>
2019-07-11 17:31:26 -0400

    Update WTUA;ENVHEALTH!HOUSING!INV!~.js

--- Scripts/Event/WTUA;ENVHEALTH!HOUSING!INV!~.js
+++ Scripts/Event/WTUA;ENVHEALTH!HOUSING!INV!~.js
@@ -1,8 +1,7 @@
 // WTUA;ENVHEALTH!HOUSING!INV!~
-if (wfTask == 'Inspection' && wfStatus == 'Reinspect' && AInfo['Mosquito Control'] != 'Yes' && AInfo['Reinspection Date'] != null && AInfo['Reinspection Date'] != '') {
-	scheduleInspectDate('Reinspection',AInfo['Reinspection Date'],AInfo['Assigned To']);
+if (wfTask == 'Inspection' && wfStatus == 'Reinspect'&& getTSIfieldValue('Reinspection Date', 'Inspection') != null) {
+	scheduleInspectDate('Reinspection',getTSIfieldValue('Reinspection Date', 'Inspection'),assignedEHS);
 	}
-
-if (wfTask == 'Inspection' && wfStatus == 'Reinspect' && AInfo['Mosquito Control'] == 'Yes' && AInfo['Reinspection Date'] != null && AInfo['Reinspection Date'] != '') {
-	scheduleInspectDate('Reinspection',AInfo['Reinspection Date'],'LLOBDELL');
+if (wfTask == 'Inspection' && wfStatus == 'Reinspect' && getTSIfieldValue('Reinspection Date', 'Initial Processing') == null) {
+	scheduleInspectDate('Reinspection',nextWorkDay(dateAdd(null,1)),AInfo['Assigned To']); 
 	}
