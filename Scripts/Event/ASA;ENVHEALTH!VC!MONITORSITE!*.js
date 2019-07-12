b9cc416891d3aea9304c823147e60029cf0efa52
Richard Voller <rvoller@hhcorp.org>
2019-07-12 14:42:04 -0400

    Update ASA;ENVHEALTH!VC!MONITORSITE!*.js

--- Scripts/Event/ASA;ENVHEALTH!VC!MONITORSITE!*.js
+++ Scripts/Event/ASA;ENVHEALTH!VC!MONITORSITE!*.js
@@ -3,7 +3,7 @@ copyParcelGisObjects();
 var aZone = getVectorZone(capId);
 var getLat = "1.0"; //Need to automate to the correct value
 var getLong = "1.0"; //Need to automate to the correct value
-var techByZone = 'RMERCADO@HAHPDC1.HHCORP.ORG';
+var techByZone = hhcgetUserByDiscipline('VCMosquito');
 assignCap(techByZone);
 editAppSpecific("Zone",aZone);
 //editAppSpecific("Latitude",getLat);
