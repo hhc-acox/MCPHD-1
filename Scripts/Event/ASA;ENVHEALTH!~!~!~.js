06d8308d9532f74a0e55b9883f0cd31e5d2e79c9
Richard Voller <rvoller@hhcorp.org>
2019-07-12 16:03:57 -0400

    Update ASA;ENVHEALTH!~!~!~.js

--- Scripts/Event/ASA;ENVHEALTH!~!~!~.js
+++ Scripts/Event/ASA;ENVHEALTH!~!~!~.js
@@ -1,4 +1,4 @@
-//ASA;ENVHEALTH!~!~!~2
+//ASA;ENVHEALTH!~!~!~
 //lwacht: 151016: updating so it doesn't throw an error
 showDebug = true;
 showMessage = true;
@@ -13,18 +13,11 @@ comment('the census tract is: '+censusTract);
 //The rest of the departments will have to be added to this section if they use the Census Tract method to assign Work.
 //Also, new Census Tract tables need to be created for additional departments.
 //Manually Entered EHS during case creation.  Method used if department has not been setup.
-//if (AInfo['Assigned To'] != null) {
-//	areaInspector = AInfo['Assigned To'];
-//	}
+
 //Housing EHS
 if (matches(appTypeArray[1],'EHSM','HHECMSC','Housing') && (matches(appTypeArray[2],'HSG','TRA','VEH','INV','SEC'))) {
 	areaInspector = lookup('Census - Housing EHS',censusTract); 
 	logDebug('Inspector to Assign: '+areaInspector);
-	comment('Inspector to Assign: '+areaInspector);
-	//var aInsp = convertForAssignedTo(areaInspector);
-	//editAppSpecific('Assigned To',aInsp);
-	//editAppSpecific('Previous Assigned To',aInsp);
-	//assignCap(areaInspector);
 	}
 
 //Healthy Homes EHS	
@@ -35,42 +28,42 @@ if (matches(appTypeArray[2],'LHH','LINV')) {
 
 //Asthma EHS
 if (matches(appTypeArray[2],'ASP')) {
-	areaInspector = lookup('Census - Lead EHS',censusTract);
+	areaInspector = hhcgetUserByDiscipline('HHCESMCAsthma');
 	comment('the Asthma area Inspector is: '+areaInspector);
 	}
 	
 //BedBugs EHS
 if (matches(appTypeArray[2],'BBE')) {
-	areaInspector = 'LLOBDELL';
+	areaInspector = hhcgetUserByDiscipline('HHCESMCBedBugs');
 	comment('the BedBugs area Inspector is: '+areaInspector);
 	}
 	
 //CPS EHS
 if (matches(appTypeArray[2],'CPS')) {
-	areaInspector = lookup('Census - Lead EHS',censusTract);
-	comment('the Asthma area Inspector is: '+areaInspector);
+	areaInspector = hhcgetUserByDiscipline('HHCESMCConsumerProductSafety');
+	comment('the CPS area Inspector is: '+areaInspector);
 	}
 	
 //Radon EHS
 if (matches(appTypeArray[2],'RAD')) {
-	areaInspector = lookup('Census - Lead EHS',censusTract);
+	areaInspector = hhcgetUserByDiscipline('HHCESMCRadon');
 	comment('the Radon area Inspector is: '+areaInspector);
 	}	
 
-	//Senior Care EHS
+//Senior Care EHS
 if (matches(appTypeArray[2],'SCM')) {
-	areaInspector = lookup('Census - Lead EHS',censusTract);
+	areaInspector = hhcgetUserByDiscipline('HHCESMCSeniorCare');
 	comment('the Senior Care area Inspector is: '+areaInspector);
 	}	
 
 //LINV EHS
 if (AInfo['Assigned To'] == null && AInfo['Asthma'] == 'CHECKED') {
-	areaInspector = lookup('Census - Lead EHS',censusTract);
+	areaInspector = hhcgetUserByDiscipline('HHCESMCAsthma');
 	comment('the LINV is for Asthma: '+areaInspector);
 	}
 	
 if (AInfo['Assigned To'] == null && AInfo['Bed Bugs'] == 'CHECKED') {
-	areaInspector = 'LLOBDELL';
+	areaInspector = hhcgetUserByDiscipline('HHCESMCBedBugs');
 	comment('the LINV is for BedBugs: '+areaInspector);
 	}
 
@@ -80,7 +73,7 @@ if (AInfo['Assigned To'] == null && AInfo['Suspect Lead'] == 'CHECKED') {
 	}
 
 if (AInfo['Assigned To'] == null && AInfo['Consumer Product Safety'] == 'CHECKED') {
-	areaInspector = 'QWILSON';
+	areaInspector = hhcgetUserByDiscipline('HHCESMCConsumerProductSafety');
 	comment('the LINV is for Consumer Product Safety: '+areaInspector);
 	}
 	
@@ -90,19 +83,14 @@ if (AInfo['Assigned To'] == null && AInfo['Healthy Homes'] == 'CHECKED') {
 	}
 
 if (AInfo['Assigned To'] == null && AInfo['Radon'] == 'CHECKED') {
-	areaInspector = lookup('Census - Lead EHS',censusTract);
+	areaInspector = hhcgetUserByDiscipline('HHCESMCRadon');
 	comment('the LINV is for Radon: '+areaInspector);
 	}
 
 	if (AInfo['Assigned To'] == null && AInfo['Senior Care'] == 'CHECKED') {
-	areaInspector = lookup('Census - Lead EHS',censusTract);
+	areaInspector = hhcgetUserByDiscipline('HHCESMCSeniorCare');
 	comment('the LINV is for Senior Care: '+areaInspector);
 	}
-
-//if (AInfo['Assigned To'] != null) {
-//	areaInspector = AInfo['Assigned To'];
-//	}
-	
 //lwacht: 151016: updating so it doesn't throw an error
 if(areaInspector) {
 	var aInsp = convertForAssignedTo(areaInspector);
@@ -111,17 +99,12 @@ if(areaInspector) {
 	assignCap(areaInspector);
 	}
 //lwacht: 151016: end
-
-//Inspection Scheduling - based on Inspection Type.  
-// 7.5.17 chaas: no custom fields in any of these three GENERAL custom field subgroups for Mosquito Control
-
 //LINV Initial Inspection Scheduling set for next business day and case assignments.
 if (matches(appTypeArray[2],'LINV')) {
 	updateAppStatus('Open','Initial status');
 	editAppSpecific('Initial Inspection Date',nextWorkDay());
 	scheduleInspectDate('Initial Lead Inspection',nextWorkDay(),areaInspector);
 	}
-
 //Get the Initial Inspection Date and reformat it for resulting the Initial Inspection
 if (matches(appTypeArray[2],'VEH','HSG','SEC','TRA','LHH')) {
 	theDate = AInfo['Initial Inspection Date'].substring(6,10) + '-' + AInfo['Initial Inspection Date'].substring(0,2) + '-' + AInfo['Initial Inspection Date'].substring(3,5);
@@ -155,20 +138,12 @@ if (matches(appTypeArray[2],'LHH') && AInfo['Initial Inspection Date'] != null) 
 //Set the address to the Application Name field on the record
 HHC_GET_ADDRESS();
 
-// 7.5.17 chaas: SEC subtype is not in MCPHD configuration
 if (matches(appTypeArray[2],'VEH','HSG','SEC','TRA','LHH')) {
-	updateAppStatus('In Violation','Initial status');
+	updateAppStatus('In Violation','Initial Status');
 	}
 
 if (areaInspector == null || areaInspector == 'undefined') {
 	overrideMessage = 'The EHS Inspector could not be determined. Speak to a System Administrator to resolve the problem.<BR><BR>';
 	}
 
-//copy the gis object onto the record so that all gis-related functions work
-// try{
-//	copyParcelGisObjects(); 
-//}catch (err){
-//	logDebug("A JavaScript Error occurred: ASA:*/*/*/*: Copy GIS Object: " + err.message);
-//	logDebug(err.stack)
-//} 
  	
