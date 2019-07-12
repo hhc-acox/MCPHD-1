c0b6109469d148606eff014956bc629f83e2a3b1
Richard Voller <rvoller@hhcorp.org>
2019-07-12 14:27:31 -0400

    Create ASA;ENVHEALTH!VC!COMPLAINT!BEES.js

---
+++ Scripts/Event/ASA;ENVHEALTH!VC!COMPLAINT!BEES.js
@@ -0,0 +1,8 @@
+ASA;ENVHEALTH!VC!COMPLAINT!BEES.js
+copyParcelGisObjects();
+var aZone = getVectorZone(capId);
+var techByZone = hhcgetUserByDiscipline('VCBees');
+scheduleInspectDate("Bees Inspection",nextWorkDay(dateAdd(null,0,"Scheduled by script")),techByZone);
+editAppSpecific("Zone",aZone);
+assignCap(techByZone);
+updateAppStatus("Assigned");
