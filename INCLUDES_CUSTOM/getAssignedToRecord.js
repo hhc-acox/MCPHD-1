bbc19273ef7da544a920a995991be72a0bee7807
Richard Voller <rvoller@hhcorp.org>
2019-07-13 19:24:11 -0400

    Add files via upload

---
+++ INCLUDES_CUSTOM/getAssignedToRecord.js
@@ -0,0 +1,12 @@
+	function getAssignedToRecord() {
+	try {
+	cap = aa.cap.getCapDetail(capId).getOutput();
+	var capAssignPerson = cap.getAsgnStaff();
+	return capAssignPerson;
+			}
+	catch(err)
+	{
+		logDebug("A JavaScript Error occurred: getAssignedToRecord:  " + err.message);
+		logDebug(err.stack);
+	}
+}
