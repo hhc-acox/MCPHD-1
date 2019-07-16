0a0d9b77e8160bd7e2bb23c065f3552ad7a648e5
Richard Voller <rvoller@hhcorp.org>
2019-07-16 14:37:45 -0400

    Add files via upload

---
+++ INCLUDES_CUSTOM/HHC_CREATE_RCP_CASE.js
@@ -0,0 +1,27 @@
+function HHC_CREATE_RCP_CASE() {
+	try{
+		showMessage = true;
+		var saveID = capId;
+		var currDate = new Date();
+		var asgnTo = getAppSpecific('Assigned To');
+		currDate = dateAdd(null,0);
+		var crtOrder = 'No';
+		var aStatus = getAppStatus();
+		var isItPM = 'No';
+		if(isTaskStatus('Requesting Admin Court Order','Admin Court Order Obtained')){crtOrder = 'Yes';}
+		if (aStatus = 'Permanent Injuction'){isItPM = 'Yes';}
+		newChildID = createChild('EnvHealth','EHSM','RCP','NA','');
+		copyAppSpecific(newChildID);
+		editAppSpecific('TRA Case',capIDString,newChildID);
+		editAppSpecific('Assigned To EHS',asgnTo,newChildID);
+		editAppSpecific('Referral Date',currDate,newChildID);
+		editAppSpecific('Court Order',crtOrder,newChildID);
+		editAppSpecific('Permanent Injunction',isItPM,newChildID);
+		copyASITables(saveID,newChildID);
+		HHC_GET_ADDRESS_FOR_CHILD();	
+	}
+	catch(err){
+		logDebug("A JavaScript Error occurred: HHC_CREATE_RCP_CASE:  " + err.message);
+		logDebug(err.stack);
+	}
+}
