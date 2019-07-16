293a75e02895ec2cba65bd839fcd7490b382daa9
Richard Voller <rvoller@hhcorp.org>
2019-07-16 12:26:02 -0400

    Update IRSA;EnvHealth!Radon!ServiceRequest!~.js

--- Scripts/Event/IRSA;EnvHealth!Radon!ServiceRequest!~.js
+++ Scripts/Event/IRSA;EnvHealth!Radon!ServiceRequest!~.js
@@ -1,18 +1,29 @@
 //IRSA:EnvHealth/Radon/ServiceRequest/*
 var assignedTo = getAssignedToRecord();
-if(checkInspectionResult('Radon Detector Placement','Unable to Place')) {
-	scheduleInspectDate('Radon Detector Placement',nextWorkDay(dateAdd(null,2)),assignedTo);  //Needs a method to use the recheck date field to get the date
-	}
+var theDate = hhcgetInspRecheckDate(capId,inspId);
+if(checkInspectionResult('Radon Detector Placement','Unable to Place') && theDate == null) {
+	scheduleInspectDate('Radon Detector Placement',nextWorkDay(dateAdd(null,0)),assignedTo);  //When there is no Recheck Date
+}
+if(checkInspectionResult('Radon Detector Placement','Unable to Place') && theDate != null) {
+	scheduleInspectDate('Radon Detector Placement',theDate,assignedTo);  //When there is a Recheck Date
+}
 if(checkInspectionResult('Radon Detector Placement','Unable to Place - Case Closed')) {
 	updateTask('Initial Visit','Case Closed Unable to Place Detector','Updated by Script');
 	}
-if(checkInspectionResult('Radon Detector Placement','Placed')) {
-	scheduleInspectDate('Radon Detector Retrieval',nextWorkDay(dateAdd(null,2)),assignedTo);  //Needs a method to use the recheck date field to get the date
+if(checkInspectionResult('Radon Detector Placement','Placed') && theDate == null) {
+	scheduleInspectDate('Radon Detector Retrieval',nextWorkDay(dateAdd(null,0)),assignedTo);  //When there is no Recheck Date
 	updateTask('Initial Visit','Placed Detector','Updated by Script');
-	}
-if(checkInspectionResult('Radon Detector Retrieval','Unable to Retrieve')) {
-	scheduleInspectDate('Radon Detector Retrieval',nextWorkDay(dateAdd(null,2)),assignedTo);  //Needs a method to use the recheck date field to get the date
-	}
+}
+if(checkInspectionResult('Radon Detector Placement','Placed') && theDate != null) {
+	scheduleInspectDate('Radon Detector Retrieval',theDate,assignedTo);  //When there is a Recheck Date
+	updateTask('Initial Visit','Placed Detector','Updated by Script');
+}
+if(checkInspectionResult('Radon Detector Placement','Unable to Retrieve') && theDate == null) {
+	scheduleInspectDate('Radon Detector Retrieval',nextWorkDay(dateAdd(null,0)),assignedTo);  //When there is no Recheck Date
+}
+if(checkInspectionResult('Radon Detector Placement','Unable to Retrieve') && theDate != null) {
+	scheduleInspectDate('Radon Detector Retrieval',theDate,assignedTo);  //When there is a Recheck Date
+}
 if(checkInspectionResult('Radon Detector Retrieval','Retrieved')) {
 	updateTask('Followup Visit','Picked up Detector','Updated by Script');
 	}
