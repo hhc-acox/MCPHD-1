b12e1ac51185b69590e04c43d7270b0038bfc888
Richard Voller <rvoller@hhcorp.org>
2019-07-16 14:51:36 -0400

    Update HHC_CREATE_RCP_CASE.js

--- INCLUDES_CUSTOM/HHC_CREATE_RCP_CASE.js
+++ INCLUDES_CUSTOM/HHC_CREATE_RCP_CASE.js
@@ -6,17 +6,17 @@ function HHC_CREATE_RCP_CASE() {
 		var asgnTo = getAppSpecific('Assigned To');
 		currDate = dateAdd(null,0);
 		var crtOrder = 'No';
-		var aStatus = getAppStatus();
-		var isItPM = 'No';
+		//var aStatus = getAppStatus();
+		//var isItPM = 'No';
 		if(isTaskStatus('Requesting Admin Court Order','Admin Court Order Obtained')){crtOrder = 'Yes';}
-		if (aStatus = 'Permanent Injuction'){isItPM = 'Yes';}
+		//if (aStatus = 'Permanent Injuction'){isItPM = 'Yes';}
 		newChildID = createChild('EnvHealth','EHSM','RCP','NA','');
 		copyAppSpecific(newChildID);
 		editAppSpecific('TRA Case',capIDString,newChildID);
 		editAppSpecific('Assigned To EHS',asgnTo,newChildID);
 		editAppSpecific('Referral Date',currDate,newChildID);
 		editAppSpecific('Court Order',crtOrder,newChildID);
-		editAppSpecific('Permanent Injunction',isItPM,newChildID);
+		//editAppSpecific('Permanent Injunction',isItPM,newChildID);
 		copyASITables(saveID,newChildID);
 		HHC_GET_ADDRESS_FOR_CHILD();	
 	}
