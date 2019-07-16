001e674ee2949410a16e974b11f305229fa3b775
Richard Voller <rvoller@hhcorp.org>
2019-07-16 11:59:16 -0400

    Update hhcgetInspRecheckDate.js

--- INCLUDES_CUSTOM/hhcgetInspRecheckDate.js
+++ INCLUDES_CUSTOM/hhcgetInspRecheckDate.js
@@ -1,34 +1,11 @@
-function hhcgetInspRecheckDate(inspId) {
+function hhcgetInspRecheckDate(capId,inspId) {
    try {
-	   if (inspId == 'undefined' || inspId == null) {
-		   inspId = 0;
-	   }
-       var initialContext = aa.proxyInvoker.newInstance("javax.naming.InitialContext", null).getOutput();
-       var ds = initialContext.lookup("java:/AA");
-       var conn = ds.getConnection();
-       // Get the Recheck date using SQL
-       var selectString = "select G6_DESI_DD from G6ACTION where G6_ACT_NUM = '" + inspId + "'";
-       //aa.print(selectString)
-       var SQLStatement = conn.prepareStatement(selectString);
-       var rSet = SQLStatement.executeQuery();
-       var arrDatelist = new Array();
-       while (rSet.next()) {
-          
-           aa.print(rSet.getString("G6_DESI_DD"));
-           arrDatelist.push(rSet.getString("G6_DESI_DD"))
-       }
-       return arrDatelist[0];
+	   var l = aa.inspection.getInspection(capId,inspId).getOutput();
+	   inspObj = l.getInspection().getActivity();
+	   var theDate = inspObj.getDesiredDate();
+	   return theDate;
    }
-   catch (e) {
-       aa.print("Exception getting data from G6ACTION: " + e.message);
-   }
-   finally {
-	    if (rSet != null || rSet != 'undefined') try { rSet.close(); } catch (e) {aa.print("Exception getting data from rSet: " + e.message); } 
-	    if (SQLStatement != null || rSet != 'undefined') try { SQLStatement.close(); } catch (e) {aa.print("Exception getting data from SQLStatement: " + e.message); }
-	    if (conn != null || rSet != 'undefined') try { conn.close(); } catch (e) {aa.print("Exception getting data from conn: " + e.message); }
-        if (initialContext != null || rSet != 'undefined') try { initialContext.close(); } catch (e) {aa.print("Exception getting data from initialContext: " + e.message); }
-           
+      catch (e) {
+       aa.print("Exception getting data from hhcgetInspRecheckDate2: " + e.message);
    }
 }
-
-//var mytest = getRecheckDate(30516045);
