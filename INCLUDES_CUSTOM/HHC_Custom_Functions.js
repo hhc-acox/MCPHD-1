8083fd084eacd0a7b828c7e1c57a826c34878ddc
Richard Voller <rvoller@hhcorp.org>
2019-08-05 14:21:15 -0400

    Update HHC_Custom_Functions.js

--- INCLUDES_CUSTOM/HHC_Custom_Functions.js
+++ INCLUDES_CUSTOM/HHC_Custom_Functions.js
@@ -1,38 +1,3 @@
-function HHC_GET_ADDRESS() 
-{
-	try{
-
-		var addrResult = aa.address.getAddressByCapId(capId);
-		var addrArray = new Array();
-		var addrArray = addrResult.getOutput();
-		var streetName = addrArray[0].getStreetName();
-		var hseNum = addrArray[0].getHouseNumberStart();
-		var streetSuffix = addrArray[0].getStreetSuffix();
-		var streetDir = addrArray[0].getStreetDirection();
-			if (streetSuffix == null || streetSuffix == '' || streetSuffix == 'undefined') {
-				streetSuffix = ' ';
-				}
-
-			if (streetDir == null || streetDir == '' || streetDir == 'undefined') {
-				streetDir == ' ';
-				}
-
-			comment ('The Address is: '+hseNum+' '+streetDir+' '+streetName+' '+streetSuffix);
-			if (streetDir == null) {
-				editAppName(hseNum+' '+streetName+' '+streetSuffix);
-				}
-
-			if (streetDir != null) {
-				editAppName(hseNum+' '+streetDir+' '+streetName+' '+streetSuffix);
-				}
-
-			}
-	catch(err)
-		{
-			logDebug("A JavaScript Error occurred: HHC_GET_ADDRESS:  " + err.message);
-			logDebug(err.stack);
-		}
-}
 function HHC_GET_ADDRESS_FOR_CHILD() 
 {
 	try{
