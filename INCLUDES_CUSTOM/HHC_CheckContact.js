d72bd41e72da62d46d44643f02c962eab1b7d0dc
Richard Voller <rvoller@hhcorp.org>
2019-08-05 14:18:48 -0400

    Add files via upload

---
+++ INCLUDES_CUSTOM/HHC_CheckContact.js
@@ -0,0 +1,28 @@
+function HHC_CheckContact() 
+{
+	try{
+		showMessage=true;
+		appName = cTempAry[yy][0];
+		cContactTypeToCheckFor = cTempAry[yy][1];
+		cContactSeqNum = cTempAry[yy][2];
+			if (appName != prevName && matches(cContactTypeToCheckFor, 'Property Owner', 'Owner/Operator', 'Facility Owner','Business Owner','Tenant')) {
+				addCourtCase = true;
+			}
+
+			if (addCourtCase) {
+				y++;
+			}
+
+		nextNameArr[yy] = [[y],[appName],[cContactTypeToCheckFor],[cContactSeqNum]];
+		comment(nextNameArr[yy][0]+' - '+nextNameArr[yy][1]+' - '+nextNameArr[yy][2]+' - '+nextNameArr[yy][3]);
+		prevName=String(appName);
+		addCourtCase = false;
+		cContactTypeToCheckFor = '';
+
+		}	
+	catch(err)
+		{
+			logDebug("A JavaScript Error occurred: HHC_CheckContact:  " + err.message);
+			logDebug(err.stack);
+		}
+}
