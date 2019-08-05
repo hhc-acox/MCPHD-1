69c1cee153b5fb25dcb9cde614b1f02379a33341
Richard Voller <rvoller@hhcorp.org>
2019-08-05 14:05:36 -0400

    Update HHC_Custom_Functions.js

--- INCLUDES_CUSTOM/HHC_Custom_Functions.js
+++ INCLUDES_CUSTOM/HHC_Custom_Functions.js
@@ -1,15 +1,3 @@
-function HHC_CREATE_COURT(){
-	try{
-		HHC_ODYSSEY_PROCESS();
-		crtConAry=nextNameArr;
-		concnt = y;
-		HHC_CREATE_CRT_CASES();
-			}
-	catch(err){
-	logDebug("A JavaScript Error occurred: HHC_CREATE_COURT:  " + err.message);
-	logDebug(err.stack);
-				}
-}
 function HHC_ODYSSEY_PROCESS() 
 {
 	try{
