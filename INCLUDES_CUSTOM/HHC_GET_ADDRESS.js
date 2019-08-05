7944ce2969fc9b83ed410c418a82306eb6a38a69
Richard Voller <rvoller@hhcorp.org>
2019-08-05 14:22:04 -0400

    Add files via upload

---
+++ INCLUDES_CUSTOM/HHC_GET_ADDRESS.js
@@ -0,0 +1,35 @@
+function HHC_GET_ADDRESS() 
+{
+	try{
+
+		var addrResult = aa.address.getAddressByCapId(capId);
+		var addrArray = new Array();
+		var addrArray = addrResult.getOutput();
+		var streetName = addrArray[0].getStreetName();
+		var hseNum = addrArray[0].getHouseNumberStart();
+		var streetSuffix = addrArray[0].getStreetSuffix();
+		var streetDir = addrArray[0].getStreetDirection();
+			if (streetSuffix == null || streetSuffix == '' || streetSuffix == 'undefined') {
+				streetSuffix = ' ';
+				}
+
+			if (streetDir == null || streetDir == '' || streetDir == 'undefined') {
+				streetDir == ' ';
+				}
+
+			comment ('The Address is: '+hseNum+' '+streetDir+' '+streetName+' '+streetSuffix);
+			if (streetDir == null) {
+				editAppName(hseNum+' '+streetName+' '+streetSuffix);
+				}
+
+			if (streetDir != null) {
+				editAppName(hseNum+' '+streetDir+' '+streetName+' '+streetSuffix);
+				}
+
+			}
+	catch(err)
+		{
+			logDebug("A JavaScript Error occurred: HHC_GET_ADDRESS:  " + err.message);
+			logDebug(err.stack);
+		}
+}
