0aaf8a0d4edfc81dd500ed01f1d1832995b170cc
Richard Voller <rvoller@hhcorp.org>
2019-08-05 14:16:35 -0400

    Update HHC_Custom_Functions.js

--- INCLUDES_CUSTOM/HHC_Custom_Functions.js
+++ INCLUDES_CUSTOM/HHC_Custom_Functions.js
@@ -1,20 +1,3 @@
-function HHC_SORT_CONTACTS() 
-{
-	try{
-		showMessage=true;
-		appName = cContactAry[yy].getCapContactModel().getPeople().getFullName();
-		appConType = cContactAry[yy].getCapContactModel().getContactType();
-		appSeqNum = cContactAry[yy].getCapContactModel().getPeople().getContactSeqNumber();
-		cTempAry[yy] = [[appName],[appConType],[appSeqNum]];
-		cTempAry.sort();
-		comment(cTempAry[yy][0]+' - '+cTempAry[yy][1]+' - '+cTempAry[yy][2]);
-			}
-	catch(err)
-		{
-			logDebug("A JavaScript Error occurred: HHC_SORT_CONTACTS:  " + err.message);
-			logDebug(err.stack);
-			}
-}
 function HHC_CheckContact() 
 {
 	try{
