8494d968f4ae4c3ab1f1f62ec99c4031a1e7f2d1
Richard Voller <rvoller@hhcorp.org>
2019-08-05 14:19:29 -0400

    Update HHC_Custom_Functions.js

--- INCLUDES_CUSTOM/HHC_Custom_Functions.js
+++ INCLUDES_CUSTOM/HHC_Custom_Functions.js
@@ -1,33 +1,3 @@
-function HHC_VIOLATIONS_LOOP() 
-{
-	try{
-		loadASITable('VIOLATIONS');
-			if (tableHasRows('VIOLATIONS')) {
-				fixVIOLATIONS = loadASITable('VIOLATIONS');
-				removeASITable('VIOLATIONS');
-			}
-			if (tableHasRows('VIOLATIONS')) {
-				for(i in fixVIOLATIONS) {
-					eachrow = fixVIOLATIONS[i];
-					if (matches(eachrow['Status'],'Open', 'Court')) {
-						fixVIOLATIONS[i]['Status'] ='Final';
-					}
-
-				}
-
-			}
-
-		if (tableHasRows('VIOLATIONS')) {
-			addASITable('VIOLATIONS',fixVIOLATIONS);
-		}
-	}
-	catch(err)
-	{
-		logDebug("A JavaScript Error occurred: HHC_VIOLATIONS_LOOP:  " + err.message);
-		logDebug(err.stack);
-	}
-}
-	
 function HHC_GET_ADDRESS() 
 {
 	try{
