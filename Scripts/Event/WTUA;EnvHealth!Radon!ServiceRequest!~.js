cbc83902f51a47b0167fb822a4b5d9a4cce43588
emse30 <bleblanc@hhcorp.org>
2019-08-05 13:19:03 -0400

    commit by emse tools 2019-08-05 13:19:02

--- Scripts/Event/WTUA;EnvHealth!Radon!ServiceRequest!~.js
+++ Scripts/Event/WTUA;EnvHealth!Radon!ServiceRequest!~.js
@@ -1,4 +1,4 @@
-//WTUA:EnvHealth/Radon/ServiceRequest/*
+//WTUA;EnvHealth!Radon!ServiceRequest!~
 var assignedTo = getAssignedToRecord();
 if (wfTask == 'Initial Visit' && matches(wfStatus,'Initial Contact') && getTSIfieldValue('Inspection Date', 'Initial Visit') != null) {
 	scheduleInspectDate('Radon Detector Placement',getTSIfieldValue('Inspection Date', 'Case Intake'), assignedTo);
