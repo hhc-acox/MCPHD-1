a82afaf9ffdfd26618e5f968eae8fa1ebd0cfe16
emse30 <bleblanc@hhcorp.org>
2019-08-07 12:05:14 -0400

    commit by emse tools 2019-08-07 12:05:13

--- Scripts/Event/ASA;ENVHEALTH!VC!COMPLAINT!BEES.js
+++ Scripts/Event/ASA;ENVHEALTH!VC!COMPLAINT!BEES.js
@@ -1,14 +1,7 @@
 //ASA:ENVHEALTH/VC/COMPLAINT/BEES
-var resultDate = sysDate;
-var resultComment = 'Updated by Script';
-var inspStatus = 'Closed';
 var aZone = getVectorZone(capId);
 var techByZone = hhcgetUserByDiscipline('VCBees');
 scheduleInspectDate("Bees Inspection",nextWorkDay(dateAdd(null,0,"Scheduled by script")),techByZone);
 editAppSpecific("Zone",aZone);
 assignCap(techByZone);
 updateAppStatus("Assigned");
-if (!matches(inspResult,'',' ', null,'undefined')) {
-aa.inspection.resultInspection(capId, inspId, inspStatus, resultDate, resultComment, currentUserID);
-updateAppStatus("Closed");
-}
