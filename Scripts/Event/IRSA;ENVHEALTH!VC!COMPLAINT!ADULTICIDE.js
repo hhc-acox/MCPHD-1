3c9681e3953af9a4adf24174dc3014315b91ad3b
Richard Voller <rvoller@hhcorp.org>
2019-08-07 15:44:29 -0400

    Update IRSA;ENVHEALTH!VC!COMPLAINT!ADULTICIDE.js

--- Scripts/Event/IRSA;ENVHEALTH!VC!COMPLAINT!ADULTICIDE.js
+++ Scripts/Event/IRSA;ENVHEALTH!VC!COMPLAINT!ADULTICIDE.js
@@ -1,4 +1,7 @@
 //IRSA;ENVHEALTH!VC!COMPLAINT!ADULTICIDE.js
+var resultDate = sysDate;
+var resultComment = 'Updated by Script';
+var inspStatus = 'Closed';
 if (!matches(inspResult,'Received')) {
 	aa.inspection.resultInspection(capId, inspId, inspStatus, resultDate, resultComment, currentUserID);
 	updateAppStatus("Closed");
