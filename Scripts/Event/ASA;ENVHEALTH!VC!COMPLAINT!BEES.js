0cbc13cc65d1dfa963c28adc53f7ba1eb30eece6
Richard Voller <rvoller@hhcorp.org>
2019-08-07 10:08:08 -0400

    Update ASA;ENVHEALTH!VC!COMPLAINT!BEES.js

--- Scripts/Event/ASA;ENVHEALTH!VC!COMPLAINT!BEES.js
+++ Scripts/Event/ASA;ENVHEALTH!VC!COMPLAINT!BEES.js
@@ -1,8 +1,14 @@
-//ASA;ENVHEALTH!VC!COMPLAINT!BEES.js
+//ASA:ENVHEALTH/VC/COMPLAINT/BEES
 copyParcelGisObjects4XAPO();
+var resultDate = sysDate;
+var resultComment = 'Updated by Script';
+var inspStatus = 'Closed';
 var aZone = getVectorZone(capId);
 var techByZone = hhcgetUserByDiscipline('VCBees');
 scheduleInspectDate("Bees Inspection",nextWorkDay(dateAdd(null,0,"Scheduled by script")),techByZone);
 editAppSpecific("Zone",aZone);
 assignCap(techByZone);
 updateAppStatus("Assigned");
+if (!matches(inspResult,'',' ', null,'undefined')) {
+aa.inspection.resultInspection(capId, inspId, inspStatus, resultDate, resultComment, currentUserID);
+updateAppStatus("Closed");
