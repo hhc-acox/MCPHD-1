6085cd05748d2066cf55fe1b19b36e6e64e25783
Lynda Wacht <lwacht@users.noreply.github.com>
2019-08-09 09:02:48 -0600

    test

--- Scripts/Pageflow/ACA_ONLOAD_COPY_CONTACTS.js
+++ Scripts/Pageflow/ACA_ONLOAD_COPY_CONTACTS.js
@@ -80,7 +80,7 @@ try {
 	if(parentCapId==null){
 		logDebug("Error finding parent capId.");
 	}else{
-		copyContacts(parentCapId, capId);
+		copyPeople(parentCapId, capId);
         var amendCapModel = aa.cap.getCapViewBySingle4ACA(capId);
         amendCapModel.getCapType().setSpecInfoCode(cap.getCapType().getSpecInfoCode());
         aa.env.setValue("CapModel", amendCapModel);
@@ -91,6 +91,132 @@ try {
 }
 		
 
+
+function copyPeople(srcCapId, targetCapId)
+{
+  //1. Get people with source CAPID.
+  var capPeoples = getPeople(srcCapId);
+  aa.print("Source Cap ID:" + srcCapId);
+
+  if (capPeoples == null || capPeoples.length == 0)
+  {
+    aa.print("Didn't get the source peoples!");
+    return;
+  }
+  //2. Get people with target CAPID.
+  var targetPeople = getPeople(targetCapId);
+  //3. Check to see which people is matched in both source and target.
+  for (loopk in capPeoples)
+  {
+    sourcePeopleModel = capPeoples[loopk];
+    //3.1 Set target CAPID to source people.
+    sourcePeopleModel.getCapContactModel().setCapID(targetCapId);
+    
+    targetPeopleModel = null;
+    //3.2 Check to see if sourcePeople exist.
+    if (targetPeople != null && targetPeople.length > 0)
+    {
+      for (loop2 in targetPeople)
+      {
+        if (isMatchPeople(sourcePeopleModel, targetPeople[loop2]))
+        {
+          targetPeopleModel = targetPeople[loop2];
+          break;
+        }
+      }
+    }
+    //3.3 It is a matched people model.
+    if (targetPeopleModel != null)
+    {
+      //3.3.1 Copy information from source to target.
+      aa.people.copyCapContactModel(sourcePeopleModel.getCapContactModel(), 
+
+      targetPeopleModel.getCapContactModel());
+      //3.3.2 Edit People with source People information. 
+      aa.people.editCapContactWithAttribute(targetPeopleModel.getCapContactModel());
+    }
+    //3.4 It is new People model.
+    else
+    {
+      //3.4.1 Create new people.
+      aa.people.createCapContactWithAttribute(sourcePeopleModel.getCapContactModel());
+    }
+  }
+}
+
+function isMatchPeople(capContactScriptModel, capContactScriptModel2)
+{
+  if (capContactScriptModel == null || capContactScriptModel2 == null)
+  {
+    return false;
+  }
+  var contactType1 = capContactScriptModel.getCapContactModel().getPeople().getContactType();
+  var contactType2 = capContactScriptModel2.getCapContactModel().getPeople().getContactType();
+  var firstName1 = capContactScriptModel.getCapContactModel().getPeople().getFirstName();
+  var firstName2 = capContactScriptModel2.getCapContactModel().getPeople().getFirstName();
+  var lastName1 = capContactScriptModel.getCapContactModel().getPeople().getLastName();
+  var lastName2 = capContactScriptModel2.getCapContactModel().getPeople().getLastName();
+  var fullName1 = capContactScriptModel.getCapContactModel().getPeople().getFullName();
+  var fullName2 = capContactScriptModel2.getCapContactModel().getPeople().getFullName();
+  if ((contactType1 == null && contactType2 != null) 
+    || (contactType1 != null && contactType2 == null))
+  {
+    return false;
+  }
+  if (contactType1 != null && !contactType1.equals(contactType2))
+  {
+    return false;
+  }
+  if ((firstName1 == null && firstName2 != null) 
+    || (firstName1 != null && firstName2 == null))
+  {
+    return false;
+  }
+  if (firstName1 != null && !firstName1.equals(firstName2))
+  {
+    return false;
+  }
+  if ((lastName1 == null && lastName2 != null) 
+    || (lastName1 != null && lastName2 == null))
+  {
+    return false;
+  }
+  if (lastName1 != null && !lastName1.equals(lastName2))
+  {
+    return false;
+  }
+  if ((fullName1 == null && fullName2 != null) 
+    || (fullName1 != null && fullName2 == null))
+  {
+    return false;
+  }
+  if (fullName1 != null && !fullName1.equals(fullName2))
+  {
+    return false;
+  }
+  return  true;
+}
+
+function getPeople(capId)
+{
+  capPeopleArr = null;
+  var s_result = aa.people.getCapContactByCapID(capId);
+  if(s_result.getSuccess())
+  {
+    capPeopleArr = s_result.getOutput();
+    if (capPeopleArr == null || capPeopleArr.length == 0)
+    {
+      aa.print("WARNING: no People on this CAP:" + capId);
+      capPeopleArr = null;
+    }
+  }
+  else
+  {
+    aa.print("ERROR: Failed to People: " + s_result.getErrorMessage());
+    capPeopleArr = null;  
+  }
+  return capPeopleArr;
+}
 
 
 
