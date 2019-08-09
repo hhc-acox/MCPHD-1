5e33c7978b2782e07237ff291e0d7a028ecdbfb7
Lynda Wacht <lwacht@users.noreply.github.com>
2019-08-09 15:31:30 -0600

    pool amendment pageflow script

---
+++ Scripts/Pageflow/ACA_BEFORE_CONTACT_VERIFY_ACA_USER.js
@@ -0,0 +1,131 @@
+/*------------------------------------------------------------------------------------------------------/
+| Program : ACA Page Flow Template.js
+| Event   : ACA Page Flow Template
+|
+| Usage   : Master Script by Accela.  See accompanying documentation and release notes.
+|
+| Client  : N/A
+| Action# : N/A
+|
+| Notes   :
+|
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| START User Configurable Parameters
+|
+|     Only variables in the following section may be changed.  If any other section is modified, this
+|     will no longer be considered a "Master" script and will not be supported in future releases.  If
+|     changes are made, please add notes above.
+/------------------------------------------------------------------------------------------------------*/
+var showMessage = false; // Set to true to see results in popup window
+var showDebug = false; // Set to true to see debug messages in popup window
+var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
+var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
+var cancel = false;
+/*------------------------------------------------------------------------------------------------------/
+| END User Configurable Parameters
+/------------------------------------------------------------------------------------------------------*/
+var startDate = new Date();
+var startTime = startDate.getTime();
+var message = ""; // Message String
+var debug = ""; // Debug String
+var br = "<BR>"; // Break Tag
+
+var useSA = false;
+var SA = null;
+var SAScript = null;
+var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_FOR_EMSE");
+if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
+	useSA = true;
+	SA = bzr.getOutput().getDescription();
+	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_INCLUDE_SCRIPT");
+	if (bzr.getSuccess()) {
+		SAScript = bzr.getOutput().getDescription();
+	}
+}
+
+if (SA) {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA));
+	eval(getScriptText(SAScript, SA));
+} else {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+}
+
+eval(getScriptText("INCLUDES_CUSTOM"));
+
+function getScriptText(vScriptName) {
+	var servProvCode = aa.getServiceProviderCode();
+	if (arguments.length > 1)
+		servProvCode = arguments[1]; // use different serv prov code
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		var emseScript = emseBiz.getScriptByPK(servProvCode, vScriptName, "ADMIN");
+		return emseScript.getScriptText() + "";
+	} catch (err) {
+		return "";
+	}
+}
+
+var cap = aa.env.getValue("CapModel");
+var parentId = cap.getParentCapID();
+capId = cap.getCapID();
+
+// page flow custom code begin
+try {	
+	var arrContacts = getPeople(capId);
+	for(cn in arrContacts){
+		sourcePeopleModel = arrContacts[cn];
+		var getUserResult = aa.publicUser.getPublicUserByEmail(sourcePeopleModel.getEmail());
+		if (!getUserResult.getSuccess()) {
+			cancel = true;
+			showMessage = true;
+			comment("This contact does not have an associated ACA user--please contact this user to set up their ACA account: " + sourcePeopleModel.getEmail()+".");
+		}
+	}
+} catch (err) {
+	handleError(err, "Page Flow Script");
+}
+
+function getPeople(capId)
+{
+  capPeopleArr = null;
+  var s_result = aa.people.getCapContactByCapID(capId);
+  if(s_result.getSuccess())
+  {
+    capPeopleArr = s_result.getOutput();
+    if (capPeopleArr == null || capPeopleArr.length == 0)
+    {
+      aa.print("WARNING: no People on this CAP:" + capId);
+      capPeopleArr = null;
+    }
+  }
+  else
+  {
+    aa.print("ERROR: Failed to People: " + s_result.getErrorMessage());
+    capPeopleArr = null;  
+  }
+  return capPeopleArr;
+}
+
+// page flow custom code end
+
+
+if (debug.indexOf("**ERROR") > 0) {
+	aa.env.setValue("ErrorCode", "1");
+	aa.env.setValue("ErrorMessage", debug);
+} else {
+	if (cancel) {
+		aa.env.setValue("ErrorCode", "-2");
+		if (showMessage)
+			aa.env.setValue("ErrorMessage", message);
+		if (showDebug)
+			aa.env.setValue("ErrorMessage", debug);
+	} else {
+		aa.env.setValue("ErrorCode", "0");
+		if (showMessage)
+			aa.env.setValue("ErrorMessage", message);
+		if (showDebug)
+			aa.env.setValue("ErrorMessage", debug);
+	}
+}
