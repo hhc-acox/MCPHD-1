e5934a38e93c05cf8e134638dcab5eab08d72564
Richard Voller <rvoller@hhcorp.org>
2019-08-12 11:55:31 -0400

    Delete WTUA;ENVHEALTH!~!~!~ .js

--- Scripts/Event/WTUA;ENVHEALTH!~!~!~ .js
+++
@@ -1,8 +0,0 @@
-// WTUA;ENVHEALTH!~!~!~ 
-if (matches(appTypeArray[1],'EHSM','HHECMSC','HOUSING')) { //07/13/17 chaas: Filter for these three appTypes only, per Lily Cheng
-if (capName == '') {
-	
-//replaced branch(ES_GET_ADDRESS)
-ES_GET_ADDRESS();
-	}
-}
