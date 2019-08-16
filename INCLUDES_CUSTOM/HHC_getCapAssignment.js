e8fff9da501378f7a3f328456256c9b9f49ed791
Richard Voller <rvoller@hhcorp.org>
2019-08-16 17:39:35 -0400

    Update HHC_getCapAssignment.js

--- INCLUDES_CUSTOM/HHC_getCapAssignment.js
+++ INCLUDES_CUSTOM/HHC_getCapAssignment.js
@@ -1,18 +1,6 @@
 function HHC_getCapAssignment(capId){
 	try {
-	var itemCap = capId
-	var cdScriptObjResult = aa.cap.getCapDetail(itemCap);
-	if (!cdScriptObjResult.getSuccess())
-		{ logDebug("**ERROR: No cap detail script object : " + cdScriptObjResult.getErrorMessage()) ; return false; }
-
-	var cdScriptObj = cdScriptObjResult.getOutput();
-
-	if (!cdScriptObj)
-		{ logDebug("**ERROR: No cap detail script object") ; return false; }
-
-	cd = cdScriptObj.getCapDetailModel();
-	var theAssignStaff = cd.getOutput();
-	var theCapAssigned = theAssignStaff.getAsgnStaff();
+	var theCapAssigned =  = capDetail.getAsgnStaff();
 	return theCapAssigned;
 	}
 		catch(err)
