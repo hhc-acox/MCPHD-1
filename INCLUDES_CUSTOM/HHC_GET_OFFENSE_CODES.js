86f0dc406e810506128be97f6eff7e69e84e30ed
Richard Voller <rvoller@hhcorp.org>
2019-08-18 17:30:50 -0400

    Update HHC_GET_OFFENSE_CODES.js

--- INCLUDES_CUSTOM/HHC_GET_OFFENSE_CODES.js
+++ INCLUDES_CUSTOM/HHC_GET_OFFENSE_CODES.js
@@ -17,6 +17,7 @@
 //3. for each value look up the corresponding codes in the translation table that fits the case and push each code set to an array:
 							//Trash Occupied - Residential - VioCode_Chpt10_Occ
 							if (parseInt(code10or19) == 10 && AInfo['Property Type'] == 'Occupied') 
+								var y = 0;
 								v = lookup('VioCode_Chpt10_Occ',crtVIOLATIONS[a]['Violation']);	
 								vioCodeNums = v.replace(/\D/g,'');
 									for(var x=0;x<vioCodeNums.length;x+5){
@@ -26,11 +27,12 @@
 									v = ''; 
 									vioCodeNums = '';
 									newVioCode = '';
+									y++;
 									}
 							//Trash on vacant lot - Residential - VioCode_Chpt10_VL
 							if (parseInt(code10or19) == 10 && matches(AInfo['Property Type'],'Vacant Lot')) 
 								v = lookup('VioCode_Chpt10_VL',crtVIOLATIONS[i]['Violation']);
-							//Trash on vacant structure - replace(/\D/g,'');esidential - VioCode_Chpt10_VS
+							//Trash on vacant structure - Residential - VioCode_Chpt10_VS
 							if (parseInt(code10or19) == 10 && matches(AInfo['Property Type'],'Vacant Structure')) 
 								v = lookup('VioCode_Chpt10_VS',crtVIOLATIONS[i]['Violation']);
 							//Trash Occupied - Commercial - VioCode_Chpt19
@@ -44,9 +46,12 @@
 	//For each row in the column:
 	  // remove the "-" and add "OI" at the end
 						}
+						
 					}
 				}
-				addASITable('OFFENSE CODES',masterArray, newChildID);
+				if(y>0)
+				addASITable('OFFENSE CODES',masterArray[0], newChildID);
+				for(var z=1; z<y; z++){addToASITable('OFFENSE CODES',masterArray[z], newChildID);}
 			}
 		catch(err)
 		{
