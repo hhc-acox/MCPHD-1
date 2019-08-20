42e6160d872780f26772a448527140cea6c0ad9e
Richard Voller <rvoller@hhcorp.org>
2019-08-20 12:35:05 -0400

    Update HHC_GET_OFFENSE_CODES.js

--- INCLUDES_CUSTOM/HHC_GET_OFFENSE_CODES.js
+++ INCLUDES_CUSTOM/HHC_GET_OFFENSE_CODES.js
@@ -83,9 +83,9 @@ function HHC_GET_OFFENSE_CODES() {
 						comment('the number of loops should be: '+newVioCode.length/5);
 						comment('the value of newVioCode is '+newVioCode);
 						comment('element 1 of newVioCode is ' +newVioCode[0]);
-						tempArray = new array();
-						tempArray = [newVioCode[0]];
-						elementArray['OFFENSE CODE'] = tempArray[0][0];
+						var z = '';
+						z = newVioCode[0];
+						elementArray['OFFENSE CODE'] = z;
 						masterArray.push(elementArray);
 						addASITable('OFFENSE CODES',masterArray, newChildID);
 
