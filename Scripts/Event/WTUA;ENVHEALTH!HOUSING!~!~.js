1d38af70de0c124d3e272ec919316b6f32b17b04
emse30 <bleblanc@hhcorp.org>
2019-08-21 10:08:27 -0400

    commit by emse tools 2019-08-21 10:08:26

--- Scripts/Event/WTUA;ENVHEALTH!HOUSING!~!~.js
+++ Scripts/Event/WTUA;ENVHEALTH!HOUSING!~!~.js
@@ -1,10 +1,1 @@
-//lwacht: 181022: #143: Fee Assessments
-try{
-	if(wfTask=="Reinspection" && wfStatus.indexOf("Ticket Issued")>-1){
-		updateFee("H_T100", "H_TRA", "FINAL", 1, "Y", "Y");
-	}
-}catch(err){
-	logDebug("A JavaScript Error occurred: WTUA:EnvHealth/Housing/*/*: Fee assessments: " + err.message);
-	logDebug(err.stack)
-}
-//lwacht: 181022: #143: end
+// Enter your script here...
