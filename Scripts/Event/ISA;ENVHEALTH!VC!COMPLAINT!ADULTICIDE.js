7a7c798fe483e731b408bb7f5e53229d0231df20
Lynda Wacht <lwacht@users.noreply.github.com>
2019-08-22 10:27:35 -0600

    moving VC adulticide code

--- Scripts/Event/ISA;ENVHEALTH!VC!COMPLAINT!ADULTICIDE.js
+++ Scripts/Event/ISA;ENVHEALTH!VC!COMPLAINT!ADULTICIDE.js
@@ -1,3 +1,4 @@
+//lwacht: 190822: moved to new record type--custom field does not currently exist, so this may not be needed.
 //lwacht: 190129: populate the adulticide custom date field when an adulticide inspection is scheduled.
 try{
 	if(inspType=="Adulticide"){
