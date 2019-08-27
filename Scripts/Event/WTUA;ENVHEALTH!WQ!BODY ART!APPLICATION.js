c6801fee9bb07d0482ce3098529ba045db037bf5
Richard Voller <rvoller@hhcorp.org>
2019-08-27 17:24:28 -0400

    Update WTUA;ENVHEALTH!WQ!BODY ART!APPLICATION.js

--- Scripts/Event/WTUA;ENVHEALTH!WQ!BODY ART!APPLICATION.js
+++ Scripts/Event/WTUA;ENVHEALTH!WQ!BODY ART!APPLICATION.js
@@ -26,16 +26,12 @@ if (wfTask == 'Issuance' && matches(wfStatus,'Issued')) {
 	//Create a Body Art License Function
 	//HHC_CREATE_BODYART_LICENSE();
 	//Create a copy inspections and guidesheets function
-/* 	var pParentCapId = parCapId;
-	var newChildID = CapId;
-	assignedInspector = HHC_getCapAssignment(pParentCapId);
-	assignCap(assignedInspector,newChildID);
-	capId = parCapId;
-	scheduleInspectDate('Initial',nextWorkDay(dateAdd(null,89)),assignedInspector);
-	copyOwner(parCapId, newChildID); */
-	comment('the capid is - '+capId);
+ 	var parId = getParent();
+	var saveId = capId;
 	assignedInspector = HHC_getCapAssignment(capId);
-	comment('assigned is - '+assignedInspector);
-	var parId = getParent();
-	comment('the parId is - '+parId);
-}
+	assignCap(assignedInspector,parId);
+	copyOwner(capId, parId); 
+	capId = parId;
+	scheduleInspectDate('Initial',nextWorkDay(dateAdd(null,89)),assignedInspector);
+	capId = saveId;
+	}
