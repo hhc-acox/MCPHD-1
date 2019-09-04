4691cfced88cb2a29386722ace93b847696c49e9
Richard Voller <rvoller@hhcorp.org>
2019-09-04 09:38:10 -0400

    Add files via upload

---
+++ INCLUDES_CUSTOM/HHC_getDepartmentPath.js
@@ -0,0 +1,13 @@
+
+function HHC_getDepartmentPath(username)
+	{
+	var suo = aa.person.getUser(username).getOutput(); 
+	var dpt = aa.people.getDepartmentList(null).getOutput();
+	for (var thisdpt in dpt)
+	  	{
+	  	var m = dpt[thisdpt]
+	  	var n = m.getServiceProviderCode() + "/" + m.getAgencyCode() + "/" + m.getBureauCode() + "/" + m.getDivisionCode() + "/" + m.getSectionCode() + "/" + m.getGroupCode() + "/" + m.getOfficeCode()
+		if (n.equals(suo.deptOfUser)) 
+	  	return(n)
+  		}
+  	}
