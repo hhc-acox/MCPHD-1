219157d9d286c5956de117424f6926fde5d473ed
Richard Voller <rvoller@hhcorp.org>
2019-09-04 12:21:46 -0400

    Add files via upload

---
+++ INCLUDES_CUSTOM/HHC_getMyDepartment.js
@@ -0,0 +1,22 @@
+function HHC_getMyDepartment(username)
+	{
+		try{
+			var suo = aa.person.getUser(username).getOutput(); 
+			var dpt = aa.people.getDepartmentList(null).getOutput();
+			var suof = suo.deptOfUser;
+			for (var thisdpt in dpt)
+				{
+				var m = dpt[thisdpt];
+				var p = m.getDeptName();
+				var n = m.getServiceProviderCode() + "/" + m.getAgencyCode() + "/" + m.getBureauCode() + "/" + m.getDivisionCode() + "/" + m.getSectionCode() + "/" + m.getGroupCode() + "/" + m.getOfficeCode();
+					if (n == suof){
+							return(p);		
+					}		
+				}
+			}
+		catch(err)
+		{
+		logDebug("A JavaScript Error occurred: HHC_getMyDepartment:  " + err.message);
+		logDebug(err.stack);
+		}
+	}
