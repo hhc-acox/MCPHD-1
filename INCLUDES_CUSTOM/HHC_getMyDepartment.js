104fa4bc662b2d25de6e2cd7c671b36caf499a83
Richard Voller <rvoller@hhcorp.org>
2019-10-31 21:23:33 -0400

    Update HHC_getMyDepartment.js

--- INCLUDES_CUSTOM/HHC_getMyDepartment.js
+++ INCLUDES_CUSTOM/HHC_getMyDepartment.js
@@ -4,25 +4,25 @@ function HHC_getMyDepartment(username)
 			if (username != null) {
 			var suo = aa.person.getUser(username).getOutput(); 
 			var dpt = aa.people.getDepartmentList(null).getOutput();
-			if(suo.getSuccess()){
-				var suof = '';
-				suof = suo.deptOfUser;
+			if(!suo.getSuccess()){
+				continue;
+			}
+			else{
+			var suof = suo.deptOfUser;
 			for (var thisdpt in dpt)
 				{
 				var m = dpt[thisdpt];
 				var p = m.getDeptName();
 				var n = m.getServiceProviderCode() + "/" + m.getAgencyCode() + "/" + m.getBureauCode() + "/" + m.getDivisionCode() + "/" + m.getSectionCode() + "/" + m.getGroupCode() + "/" + m.getOfficeCode();
-					if (n == suof)
-						{
-						return(p);	
+					if (n == suof){
+							return(p);	
 						}					
-				}		
+					}		
+				}
 			}
-		}
 		catch(err)
 		{
 		logDebug("A JavaScript Error occurred: HHC_getMyDepartment:  " + err.message);
 		logDebug(err.stack);
 		}
 	}
-}
