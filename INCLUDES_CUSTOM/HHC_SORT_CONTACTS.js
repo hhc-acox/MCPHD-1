2f5e2b3b0cb0c3e743a2b737d7193c45a32cfc6d
Richard Voller <rvoller@hhcorp.org>
2019-11-19 15:06:37 -0500

    Update HHC_SORT_CONTACTS.js

--- INCLUDES_CUSTOM/HHC_SORT_CONTACTS.js
+++ INCLUDES_CUSTOM/HHC_SORT_CONTACTS.js
@@ -4,10 +4,11 @@ function HHC_SORT_CONTACTS()
 		showMessage=true;
 		appName = cContactAry[yy].getCapContactModel().getPeople().getFullName();
 		appConType = cContactAry[yy].getCapContactModel().getContactType();
+		appConRelate = cContactAry[yy].getCapContactModel().getRelation();
 		appSeqNum = cContactAry[yy].getCapContactModel().getPeople().getContactSeqNumber();
-		cTempAry[yy] = [[appName],[appConType],[appSeqNum]];
+		cTempAry[yy] = [[appName],[appConType],[appConRelate],[appSeqNum]];
 		cTempAry.sort();
-		comment(cTempAry[yy][0]+' - '+cTempAry[yy][1]+' - '+cTempAry[yy][2]);
+		comment(cTempAry[yy][0]+' - '+cTempAry[yy][1]+' - '+cTempAry[yy][2]+' - '+cTempAry[yy][3]);
 			}
 	catch(err)
 		{
