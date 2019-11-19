f1147649283f067d1f3d271fc0965169897d29b6
Richard Voller <rvoller@hhcorp.org>
2019-11-19 17:01:10 -0500

    Update HHC_CheckContact.js

--- INCLUDES_CUSTOM/HHC_CheckContact.js
+++ INCLUDES_CUSTOM/HHC_CheckContact.js
@@ -5,7 +5,10 @@ function HHC_CheckContact()
 		appName = cTempAry[yy][0];
 		cContactTypeToCheckFor = cTempAry[yy][1];
 		cContactSeqNum = cTempAry[yy][2];
-			if (appName != prevName && matches(cContactTypeToCheckFor, 'Property Owner', 'Owner/Operator', 'Facility Owner','Business Owner','Tenant','Pool Construction Company')) {
+		cContactRelate = cTempAry[yy][3];
+			if (appName != prevName && (matches(cContactTypeToCheckFor, 'Deed Holder','Occupant') or cContactRelate == 'Responsible Party')) 
+			
+			{
 				addCourtCase = true;
 			}
 
@@ -13,8 +16,8 @@ function HHC_CheckContact()
 				y++;
 			}
 
-		nextNameArr[yy] = [[y],[appName],[cContactTypeToCheckFor],[cContactSeqNum]];
-		comment(nextNameArr[yy][0]+' - '+nextNameArr[yy][1]+' - '+nextNameArr[yy][2]+' - '+nextNameArr[yy][3]);
+		nextNameArr[yy] = [[y],[appName],[cContactTypeToCheckFor],[cContactSeqNum],[cContactRelate]];
+		comment(nextNameArr[yy][0]+' - '+nextNameArr[yy][1]+' - '+nextNameArr[yy][2]+' - '+nextNameArr[yy][3]+' - '+nextNameArr[yy][4]);
 		prevName=String(appName);
 		addCourtCase = false;
 		cContactTypeToCheckFor = '';
