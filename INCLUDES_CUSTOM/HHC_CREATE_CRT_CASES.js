c086c9993f980aa29cb3b48d262f3211748f0c28
Richard Voller <rvoller@hhcorp.org>
2019-11-19 21:58:24 -0500

    Update HHC_CREATE_CRT_CASES.js

--- INCLUDES_CUSTOM/HHC_CREATE_CRT_CASES.js
+++ INCLUDES_CUSTOM/HHC_CREATE_CRT_CASES.js
@@ -1,53 +1,147 @@
-
-
 function HHC_CREATE_CRT_CASES() {
 	try{
 		showMessage = true;
+		comment('cases to create '+concnt);
+		fixNameArr= new Array();
+		fixNameArr=crtConAry;
+		cContactAry = new Array();
+		var cContactResult = AInfo[''];
+		cContactsExist = false;
+		ccnt=0;
 		cContactResult = aa.people.getCapContactByCapID(capId);
-		if (cContactResult.getSuccess()) {
-			cContactAry = cContactResult.getOutput();
-		}
-		if (cContactAry && cContactAry.length > 0) {
-			for (i=0; i<cContactAry.length; i++) {
-				thisContact = cContactAry[i];
-				cContactTypeToCheckFor = thisContact.getCapContactModel().getContactType();
-				cContactRelate = thisContact.getCapContactModel().getPeople().getRelation();
-				if ((matches(cContactTypeToCheckFor, 'Deed Holder','Occupant') or cContactRelate == 'Responsible Party')){
+			if (cContactResult.getSuccess()) 
+				{
+					cContactsExist = true;
+					}
+
+			if (cContactsExist) 
+				{
+					cContactAry = cContactResult.getOutput();
+					}
+
+			if (cContactsExist) 
+				{
+					for (i=0; i<concnt; i++) 
+
+					{
+					cContactResult = AInfo[''];
+					cContactsExist = false;
+					cContactAry = new Array();
+					nextNameArr = new Array();
+					prevName = 'Start';
+					cTempAry = new Array();
+					var saveID = capId;
+					comment("THE SAVEID IS "+saveID);
+					cCapContactModel = AInfo[''];
+					cContactSeqNumber = 0;
+					cPeopleModel = AInfo[''];
+					cc = 0;
+					y = 0;
+				{
 					newChildID = createChild('EnvHealth','CRT','NA','NA','');
 					copyAppSpecific(newChildID);
 					comment('New child app id = '+ newChildID);
+					masterArray = new Array();
+					elementArray = new Array();
 					code10or19 = AInfo['Ordinance Chapter'];
-					updateAppStatus("Legal Review","",newChildID);
+					updateAppStatus('Legal Review','Initial Status',newChildID);
 					assignCap('CSANDERS',newChildID);
-					editAppSpecific('Parent Case',capIDString,newChildID);				
-					if (appMatch('EnvHealth/*/LHH/*')) 	{
+					editAppSpecific('Parent Case',capIDString,newChildID);
+					
+					if (appMatch('*/*/LHH/*')) 
+					{
 						editAppSpecific('Case Type','Lead',newChildID);
+						editAppSpecific('Parent Case',saveID,newChildID);
 						editAppSpecific('EHS Court Day','THURS',newChildID);
 						editAppSpecific('EHS Court Time','1:00 PM',newChildID);
-					}
-					HHC_GET_OFFENSE_CODES(newChildID);	
-					//HHC_GET_ADDRESS_FOR_CHILD();
-					// remove the contacts createChild put on child record
-					var capContactResult = aa.people.getCapContactByCapID(newChildID);
-					if (capContactResult.getSuccess()) {
-						capContactArray = capContactResult.getOutput();
-						if (capContactArray && capContactArray.length > 0) {
-							for (var index in capContactArray) {
-								childContact = capContactArray[index];
-								aa.people.removeCapContact(newChildID, childContact.getCapContactModel().getContactSeqNumber());							}
 						}
+
+					HHC_GET_OFFENSE_CODES(newChildID);	
+					HHC_GET_ADDRESS_FOR_CHILD();
+				}	
+				ccnt++;
+				comment('ccnt = '+ccnt);
+				capId = newChildID;
+				comment("NEW CHILD ID IS "+newChildID);
+				cContactResult = aa.people.getCapContactByCapID(capId);
+				cContactAry = cContactResult.getOutput();
+				cc = cContactAry.length;
+				if (cContactResult.getSuccess()) 
+				{
+					cContactsExist = true;
 					}
-					// copy this contact to child record
-					newContact = thisContact.getCapContactModel();
-					newContact.setCapID(newChildID);
-					aa.people.createCapContact(newContact);
-				}			
+
+					if (cContactsExist) 
+					{
+						for(yy in cContactAry) 
+
+						HHC_SORT_CONTACTS();
+						}
+
+					if (cContactsExist) 
+					{
+						for(yy in cTempAry) 
+
+						HHC_CheckContact();
+						}
+
+					if (cContactsExist) 
+					{
+						for(yy in nextNameArr) 
+							
+						nextNameArr.sort();
+						}	
+						for(ii=0;ii<cc;ii++) 
+
+						{
+							var csortContactNum = nextNameArr[ii][0];
+							var csortContactNameToCheckFor = nextNameArr[ii][1];
+							var csortContactTypeToCheckFor = nextNameArr[ii][2];
+							var csortContactSeqNum = nextNameArr[ii][3];
+							var cContactRelate = nextNameArr[ii][4];
+							var cContactDelete = true;
+							cCapContactModel = cContactAry[ii].getCapContactModel();
+							if (parseInt(ccnt) == parseInt(csortContactNum)) 
+							{
+								cContactDelete = false;
+								}
+
+							if ((!matches(cContactTypeToCheckFor, 'Deed Holder','Occupant') or cContactRelate != 'Responsible Party'))	
+						    //Each person on their CRT is always Primary
+							) 
+							{
+								cContactDelete = true;
+								}
+
+							if (cContactDelete) {
+								cPeopleModel = cCapContactModel.getPeople();
+								}
+
+							if (cContactDelete) 
+							{
+								cContactSeqNumber = parseInt(csortContactSeqNum);
+								}
+
+							if (cContactDelete) 
+							{
+								aa.people.removeCapContact(capId, cContactSeqNumber);
+								}
+
+							showMessage = true;
+							comment(ccnt +' - '+nextNameArr[ii][0]+' ii= '+ii+' - '+nextNameArr[ii][1]+' - '+nextNameArr[ii][2]+' - '+nextNameArr[ii][3]+' - '+nextNameArr[ii][4]+' ---- '+cContactDelete+' - '+cContactSeqNumber+' - '+csortContactSeqNum);
+
+							}
+
+			capId = saveID;
+			comment("the saved capId is "+saveID);
+
+						}
+
+				}
 			}
-		}
+		catch(err)
+		{
+			logDebug("A JavaScript Error occurred: HHC_CREATE_CRT_CASES:  " + err.message);
+			logDebug(err.stack);
+			}
 	}
-	catch(err) {
-		logDebug("A JavaScript Error occurred: HHC_CREATE_CRT_CASES:  " + err.message);
-		logDebug(err.stack);
-	}
-}
-	
