681bab0f37c4fafe6f507b21131bf4738157a1d7
Richard Voller <rvoller@hhcorp.org>
2019-11-22 15:03:36 -0500

    Update IRMA;ENVHEALTH!HHECMSC!LINV!~.js

--- Scripts/Event/IRMA;ENVHEALTH!HHECMSC!LINV!~.js
+++ Scripts/Event/IRMA;ENVHEALTH!HHECMSC!LINV!~.js
@@ -16,3 +16,20 @@ if (isTaskActive('Inspection') && matches(inspResult,'In Violation')) {
 	editAppSpecific('Resulted in Violation','Yes');
 	updateAppStatus('Pending Case Creation');
 	}
+if (isTaskActive('Inspection') && matches(inspResult,'Unjustified')) {
+	branchTask('Inspection','Unjustified','Updated by Script');
+	}
+
+if (isTaskActive('Inspection') && matches(inspResult,'Duplicate')) {
+	branchTask('Inspection','Duplicate INV - Case Finaled','Updated by Script');
+	}
+
+if (isTaskActive('Inspection') && matches(inspResult,'Pending Order')) {
+	branchTask('Inspection','Pending Order - Case Finaled','Updated by Script');
+	}
+
+if ((inspType == 'Initial Lead Inspection') && matches(inspResult,'In Violation')) {
+	closeTask('Inspection','In Violation','Updated by Script');
+	editAppSpecific('Resulted in Violation','Yes');
+	updateAppStatus('Pending Case Creation');
+	}
