11dbfc62fe7e6b05150daebfbdcf6c423d04cddd
Richard Voller <rvoller@hhcorp.org>
2019-11-22 14:19:25 -0500

    Update IRSA;ENVHEALTH!HHECMSC!LINV.js

--- Scripts/Event/IRSA;ENVHEALTH!HHECMSC!LINV.js
+++ Scripts/Event/IRSA;ENVHEALTH!HHECMSC!LINV.js
@@ -16,3 +16,11 @@ if ((inspType == 'Initial Lead Inspection') && matches(inspResult,'In Violation'
 	editAppSpecific('Resulted in Violation','Yes');
 	updateAppStatus("Pending Case Creation");
 	}
+try{
+	if(inspType=="Reinspection"){
+		copyLeadViolations(inspId);
+	}
+}catch(err){
+	logDebug("A JavaScript Error occurred: ISA:EnvHealth/HHECMSC/LINV/NA:  " + err.message);
+	logDebug(err.stack)
+}
