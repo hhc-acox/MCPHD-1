26f3b50608e3a61cb50e8c42316808716773cba3
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2019-11-25 11:55:04 -0500

    Create getEHSMQuadrant.js

---
+++ INCLUDES_CUSTOM/getEHSMQuadrant.js
@@ -0,0 +1,20 @@
+//EHSM Quadrants
+function getEHSMQuadrant(capId){
+	var eQuadrant = "";
+	var quadrantNum = "";
+	try{
+		x = getGISInfo("MCPHD","EHSMQuadrantDistrict","quadrant");
+		quadrantNum = x.toString();
+		if (x<10){
+		eQuadrant = "EHSM Quadrant 0"+quadrantNum;
+		}
+		else
+		{eQuadrant = "EHSM Quadrant "+quadrantNum;}
+			return eQuadrant;
+			}
+	catch(err)
+	{
+		logDebug("A JavaScript Error occurred: getVectorZone:  " + err.message);
+		logDebug(err.stack);
+	}
+}
