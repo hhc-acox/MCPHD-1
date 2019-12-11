369408906c6219b9a6e962e47d6fbc7844db2217
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2019-12-11 08:45:08 -0500

    Update HHC_getMyDepartment.js

--- INCLUDES_CUSTOM/HHC_getMyDepartment.js
+++ INCLUDES_CUSTOM/HHC_getMyDepartment.js
@@ -1,25 +1,23 @@
-function HHC_getMyDepartment(username)
-	{
-		try{
-			if (username != null) {
-			var suo = aa.person.getUser(username).getOutput(); 
-			var dpt = aa.people.getDepartmentList(null).getOutput();
-			var suof = '';
-				suof = suo.deptOfUser+'';
-			for (var thisdpt in dpt)
-				{
-				var m = dpt[thisdpt];
-				var p = m.getDeptName();
-				var n = m.getServiceProviderCode() + "/" + m.getAgencyCode() + "/" + m.getBureauCode() + "/" + m.getDivisionCode() + "/" + m.getSectionCode() + "/" + m.getGroupCode() + "/" + m.getOfficeCode();
-					if (n == suof){
-							return(p);	
-						}					
-					}		
-				}
-			}
-		catch(err)
-		{
-		logDebug("A JavaScript Error occurred: HHC_getMyDepartment:  " + err.message);
-		logDebug(err.stack);
-		}
-	}
+function HHC_getMyDepartment(username) {
+    try {
+        if (username != null) {
+            var suo = aa.person.getUser(username).getOutput();
+            var dpt = aa.people.getDepartmentList(null).getOutput();
+            if (suo != null) {
+                var suof = '';
+                suof = suo.deptOfUser + '';
+                for (var thisdpt in dpt) {
+                    var m = dpt[thisdpt];
+                    var p = m.getDeptName();
+                    var n = m.getServiceProviderCode() + "/" + m.getAgencyCode() + "/" + m.getBureauCode() + "/" + m.getDivisionCode() + "/" + m.getSectionCode() + "/" + m.getGroupCode() + "/" + m.getOfficeCode();
+                    if (n == suof) {
+                        return (p);
+                    }
+                }
+            }
+        }
+    } catch (err) {
+        logDebug("A JavaScript Error occurred: HHC_getMyDepartment:  " + err.message);
+        logDebug(err.stack);
+    }
+}
