c35f2cbd00b69f76e656d08f6419823e8c85b5c5
Richard Voller <rvoller@hhcorp.org>
2019-12-13 14:35:49 -0500

    Update IRSA;ENVHEALTH!HOUSING!VEH!~.js

--- Scripts/Event/IRSA;ENVHEALTH!HOUSING!VEH!~.js
+++ Scripts/Event/IRSA;ENVHEALTH!HOUSING!VEH!~.js
@@ -4,7 +4,7 @@
 		censusTract = AInfo['ParcelAttribute.CensusTract'];
 		areaInspector = lookup('Census - Housing EHS',censusTract);
 if (isTaskActive('Reinspection') && matches(inspResult,'In Violation')) {
-	branchTask('Reinspection','In Violation','Updated by Script');
+	//closeTask('Reinspection','In Violation','Updated by Script');
 	assignTask('Additional Processing',areaInspector);
 	}
 
