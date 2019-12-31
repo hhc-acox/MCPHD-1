a9d99c4c50823713a00b91faff43a10e94c2b568
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2019-12-31 11:55:09 -0500

    Update isSupervisor.js

--- INCLUDES_CUSTOM/isSupervisor.js
+++ INCLUDES_CUSTOM/isSupervisor.js
@@ -1,10 +1,13 @@
 function isSupervisor(userID) {
-	dept = getDepartmentName(userID);
-	if (dept & dept != "") {
-		if (matches(dept, "WQ Supervisor A EHS", "WQ Supervisor A", "WQ Team A EHS", "WQ Team A Leader", "WQ Supervisor B EHS", "WQ Supervisor B", "WQ Supervisor C EHS", "WQ Supervisor C")) {
+	var suo = aa.person.getUser(userID); 
+	
+	if(suo.getSuccess()){
+                var suoOut = suo.getOutput();
+		
+		if(suoOut.officeCode == 'NA') {
 			return true;
 		}
 	}
-
+	
 	return false;
 }
