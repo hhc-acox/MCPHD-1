7ee3429e47b3d3db35ffe3ab2f6b85ecaf7475c6
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-01-14 14:30:12 -0500

    Update ASA;ENVHEALTH!~!~!~.js

--- Scripts/Event/ASA;ENVHEALTH!~!~!~.js
+++ Scripts/Event/ASA;ENVHEALTH!~!~!~.js
@@ -98,7 +98,7 @@ if (matches(appTypeArray[1],'Radon')) {
 	areaInspector = assignedAreaInspector;
 	assignCap(areaInspector);
 	if (department.indexOf('HHECMSC') > -1) {
-                closeTask('Intake', 'Accepted', 'Closed by Script', 'Closed by Script');
+                closeTask('Radon Intake', 'Accepted', 'Closed by Script', 'Closed by Script');
         }
 	comment('the Radon area Inspector is: '+areaInspector);
 	}	
