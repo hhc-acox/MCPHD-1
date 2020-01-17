a99bcb59afcd04b491af5d2f90f5dcbace6ae235
DHoops <dmhoops@gmail.com>
2020-01-17 16:53:38 -0500

    fix

--- Scripts/Event/GSUA;ENVHEALTH!WQ!POOL!LICENSE.js
+++ Scripts/Event/GSUA;ENVHEALTH!WQ!POOL!LICENSE.js
@@ -1,13 +1,19 @@
 if (GuidesheetModel && "POOL TEST RESULTS"== GuidesheetModel.getGuideType().toUpperCase()) {
-		capAssignee = capDetail.getAsgnStaff();
-		var pResult = aa.person.getUser(userName);
-		if (pResult.getSuccess()) {
-			var p = pResult.getOutput();
-			if (p != null)  {
-				toAddr = p.getEmail();
-				if (toAddr && toAddr.length > 0) {
-					eBody = "Pool Test Results checklist updated on " + capIDString;
-					aa.sendMail("no_reply_@accela.com", toAddr, "", "Checklist updated", eBody);
+		var thisCapId = GuidesheetModel.capID;
+		var tCapId = aa.cap.getCapID(thisCapId.ID1,thisCapId.ID2,thisCapId.ID3).getOutput();	
+		var tCapDetailObjResult = aa.cap.getCapDetail(tCapId);		
+		if (tCapDetailObjResult.getSuccess()) {
+			tCapDetail = capDetailObjResult.getOutput();
+			capAssignee = tCapDetail.getAsgnStaff();
+			var pResult = aa.person.getUser(userName);
+			if (pResult.getSuccess()) {
+				var p = pResult.getOutput();
+				if (p != null)  {
+					toAddr = p.getEmail();
+					if (toAddr && toAddr.length > 0) {
+						eBody = "Pool Test Results checklist updated on " + capIDString;
+						aa.sendMail("no_reply_@accela.com", toAddr, "", "Checklist updated", eBody);
+					}
 				}
 			}
 		}
