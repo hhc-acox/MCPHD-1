7edcf26772420e2e703e45e9320afbb3eb7df5ce
Richard Voller <rvoller@hhcorp.org>
2020-02-11 11:21:18 -0500

    Update IRMA;ENVHEALTH!HHECMSC!LINV!~.js

--- Scripts/Event/IRMA;ENVHEALTH!HHECMSC!LINV!~.js
+++ Scripts/Event/IRMA;ENVHEALTH!HHECMSC!LINV!~.js
@@ -1,23 +1,18 @@
-//IRMA:ENVHEALTH/HHECMSC/LINV/!
-if (isTaskActive('Inspection') && matches(inspResult,'Unjustified')) {
-	branchTask('Inspection','Unjustified','Updated by Script');
+//IRSA;ENVHEALTH!HHECMSC!LINV!~.js
+if (matches(inspType, 'Initial Lead Inspection','Reinspection') && matches(inspResult,'Unjustified')) {
+	closeTask('Closed','Complete','Unjustified - Updated by Script');
 	}
 
-if (isTaskActive('Inspection') && matches(inspResult,'Duplicate')) {
-	branchTask('Inspection','Duplicate INV - Case Finaled','Updated by Script');
-	}
-
-if (isTaskActive('Inspection') && matches(inspResult,'Pending Order')) {
-	branchTask('Inspection','Pending Order - Case Finaled','Updated by Script');
-	}
-
-if (isTaskActive('Inspection') && matches(inspResult,'In Violation')) {
-	closeTask('Inspection','In Violation','Updated by Script');
+if (matches(inspType, 'Initial Lead Inspection','Reinspection') && matches(inspResult,'In Violation')) {
+	closeTask('Closed','Complete','In Violation - Updated by Script');
 	editAppSpecific('Resulted in Violation','Yes');
 	updateAppStatus('Pending Case Creation');
 	}
-if ((inspType == 'Initial Lead Inspection') && matches(inspResult,'In Violation')) {
-	closeTask('Inspection','In Violation','Updated by Script');
-	editAppSpecific('Resulted in Violation','Yes');
-	updateAppStatus('Pending Case Creation');
+try{
+	if(inspType=="Reinspection"){
+		copyLeadViolations(inspId);
 	}
+}catch(err){
+	logDebug("A JavaScript Error occurred: ISA:EnvHealth/HHECMSC/LINV/NA:  " + err.message);
+	logDebug(err.stack)
+}
