7027ab121234f97702ca2d861522a7e031fd73b0
Richard Voller <rvoller@hhcorp.org>
2020-02-11 10:59:47 -0500

    Update IRSA;ENVHEALTH!HHECMSC!LINV!~.js

--- Scripts/Event/IRSA;ENVHEALTH!HHECMSC!LINV!~.js
+++ Scripts/Event/IRSA;ENVHEALTH!HHECMSC!LINV!~.js
@@ -1,18 +1,10 @@
 //IRSA;ENVHEALTH!HHECMSC!LINV!~.js
-if (isTaskActive('Inspection') && matches(inspResult,'Unjustified')) {
-	branchTask('Inspection','Unjustified','Updated by Script');
+if (matches(inspType, 'Initial Lead Inspection','Reinspection') && matches(inspResult,'Unjustified')) {
+	closeTask('Closed','Complete','Unjustified - Updated by Script');
 	}
 
-if (isTaskActive('Inspection') && matches(inspResult,'Duplicate')) {
-	branchTask('Inspection','Duplicate INV - Case Finaled','Updated by Script');
-	}
-
-if (isTaskActive('Inspection') && matches(inspResult,'Pending Order')) {
-	branchTask('Inspection','Pending Order - Case Finaled','Updated by Script');
-	}
-
-if ((inspType == 'Initial Lead Inspection') && matches(inspResult,'In Violation')) {
-	closeTask('Inspection','In Violation','Updated by Script');
+if (matches(inspType, 'Initial Lead Inspection','Reinspection') && matches(inspResult,'In Violation')) {
+	closeTask('Closed','Complete','In Violation - Updated by Script');
 	editAppSpecific('Resulted in Violation','Yes');
 	updateAppStatus('Pending Case Creation');
 	}
