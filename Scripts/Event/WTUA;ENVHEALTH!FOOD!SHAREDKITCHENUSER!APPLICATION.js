fd886c93ca820b8eb5d64ec258b348638b9f8de7
DHoops <dmhoops@gmail.com>
2020-02-13 12:10:26 -0500

    link SKU license with the food establishment license.

---
+++ Scripts/Event/WTUA;ENVHEALTH!FOOD!SHAREDKITCHENUSER!APPLICATION.js
@@ -0,0 +1,23 @@
+if (wfTask == "Issuance" && wfStatus == "Issued") {
+	pId = getParent();	// created by earlier script
+	logDebug("Parent id = " + pId);	// this is the SKU license
+	
+	if (pId) {
+		comInfo = loadASITable("COMMISSARY INFORMATION");
+		if (comInfo && comInfo.length > 0) {
+			for (eachRow in comInfo) {
+				licNum = comInfo[eachRow]["Commissary License"];
+				logDebug("Commissary :" + licNum);
+				licCapId = getApplication("" + licNum);
+				if (licCapId) {				
+					var result = aa.cap.createAppHierarchy(licCapId, pId); 
+					if (result.getSuccess()){
+						logDebug("Parent application successfully linked");
+					}else{
+						logDebug("Could not link applications");
+					}
+				}
+			}
+		}
+	}
+}
