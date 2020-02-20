afff102cefed9ac5b8c45e29c8eb481d3f5ef692
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-02-20 11:40:55 -0500

    Create WTUA;ENVHEALTH!EHSM!RCP!NA.js

---
+++ Scripts/Event/WTUA;ENVHEALTH!EHSM!RCP!NA.js
@@ -0,0 +1,22 @@
+if(wfTask == 'Case Intake' && wfStatus == 'Accepted') {
+    var zone = getGISInfo("MCPHD", "EHSMQuadrantDistrict", "Quadrant");
+    var assignTo = lookup('GIS - EHSM Inspector',zone); 
+    scheduleInspectDate('Evaluate',nextWorkDay(dateAdd(null,0)),assignTo);
+}
+
+try{
+                var TRARec = getParent();
+
+                logDebug("Parent ID " + TRARec.getCustomID());
+                var saveID = capId;
+                capId = TRARec;
+
+                if(((wfTask=="Final Processing" && (wfStatus=="Finaled" || wfStatus=="Paid")) || (wfTask=="Evaluation" && wfStatus=="No Work Assignment")) && TRARec){
+                    logDebug("Trying to close RCP on TRA");
+                    closeTask("Request EHSM Clean", "EHSM Cleaned", "Updated by Script", "EHSM Cleaned");
+                }
+                capId = saveID;
+                
+}catch(err){
+
+}
