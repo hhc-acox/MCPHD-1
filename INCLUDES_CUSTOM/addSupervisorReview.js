be2e48cff1acb07710cd6950fb3d525ef71a6a26
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-02-28 09:59:49 -0500

    Update addSupervisorReview.js

--- INCLUDES_CUSTOM/addSupervisorReview.js
+++ INCLUDES_CUSTOM/addSupervisorReview.js
@@ -1,56 +1,60 @@
 function addSupervisorReview(itemCap, taskType, inspID, taskName, statusName) {
-    assignTo = HHC_getMyTeamLeadersUserID(currentUserID);
-    logDebug("Assigned To: " + assignTo);
+    if (isSupervisor(currentUserID)) {
+        logDebug("User is supervisor");
+    } else {
+        assignTo = HHC_getMyTeamLeadersUserID(currentUserID);
+        logDebug("Supervisor Review for: " + assignTo);
 
-    if (taskType.toUpperCase() == "WORKFLOW") {
-		var stdWf = getStandardChoiceValues("SUPERVISOR_REVIEW_WORKFLOW");
-		for (var e in stdWf) {
-			if (appMatch(stdWf[e].value)) {
-				var wfKey = taskName + "|" + statusName;
-				var wfDisposition = taskName + "-" + statusName;
-				if (wfKey == stdWf[e].key || stdWf[e].key == '*') {
-					if (!isSupervisor(currentUserID) && !containsAcceptedAdHocTaskForDisposition(capId, wfDisposition)) {
-						addAdHocTask("ADHOC_WORKFLOW", "Supervisor Review Workflow", wfDisposition, assignTo);
-						logDebug("Added Supervisor Review Workflow task");
-						activateTask(taskName);
-						updateTask(taskName, "In Progress", "Supervisor Review in progress", "Set by script");
-					}
-				}
-			}
-		}
-    }
-    else {
-        if (taskType.toUpperCase() == "INSPECTION") {
-            if (inspID) {
-                iNumber = inspID;
-                iObjResult = aa.inspection.getInspection(capId, iNumber);
-                if (iObjResult.getSuccess()) {
-                    iObj = iObjResult.getOutput();
-                    inspType = iObj.getInspectionType();
-					var stdInsp = getStandardChoiceValues("SUPERVISOR_REVIEW_INSPECTION");
-					for (var e in stdInsp) {
-						if (appMatch(stdInsp[e].value)) {
-							if (inspType == stdInsp[e].key || stdInsp[e].key == '*') {
-                                                                var inspDate = "" + iObj.getScheduledDate().getMonth() + iObj.getScheduledDate().getDayOfMonth() + iObj.getInspectionDate().getYear();
-								var inspKey = inspDate + "-" + inspType + "-" + iObj.getInspectionStatus() + "-" + iNumber;
-								if (!isSupervisor(currentUserID) && !containsAcceptedAdHocTaskForDisposition(capId, inspKey)) {
-									addAdHocTask("ADHOC_WORKFLOW", "Supervisor Review Inspection", inspKey, assignTo);
-									logDebug("Added Supervisor Review Inspection Task");
-								}
-							}
-						}
-					}
-                }
-                else {
-                    logDebug("**ERROR retrieving inspection " + iNumber + " : " + iObjResult.getErrorMessage());
+        if (taskType.toUpperCase() == "WORKFLOW") {
+            logDebug("Workflow Supervisor Review Triggered");
+            var stdWf = getStandardChoiceValues("SUPERVISOR_REVIEW_WORKFLOW");
+            for (var e in stdWf) {
+                if (appMatch(stdWf[e].value)) {
+                    var wfKey = taskName + "|" + statusName;
+                    var wfDisposition = taskName + "-" + statusName;
+                    if (wfKey == stdWf[e].key || stdWf[e].key == '*') {
+                        if (!isSupervisor(currentUserID) && !containsAcceptedAdHocTaskForDisposition(capId, wfDisposition)) {
+                            addAdHocTask("ADHOC_WORKFLOW", "Supervisor Review Workflow", wfDisposition, assignTo);
+                            logDebug("Added Supervisor Review Workflow task");
+                            activateTask(taskName);
+                            updateTask(taskName, "In Progress", "Supervisor Review in progress", "Set by script");
+                        }
+                    }
                 }
             }
-            else {
-                logDebug("Incorrect parameters");
+        } else {
+            if (taskType.toUpperCase() == "INSPECTION") {
+                logDebug("Inspection Supervisor Review Triggered");
+                if (inspID) {
+                    iNumber = inspID;
+                    iObjResult = aa.inspection.getInspection(capId, iNumber);
+                    if (iObjResult.getSuccess()) {
+                        iObj = iObjResult.getOutput();
+                        inspType = iObj.getInspectionType();
+                        var stdInsp = getStandardChoiceValues("SUPERVISOR_REVIEW_INSPECTION");
+                        for (var e in stdInsp) {
+                            if (appMatch(stdInsp[e].value)) {
+                                if (inspType == stdInsp[e].key || stdInsp[e].key == '*') {
+                                    var inspDate = "" + iObj.getScheduledDate().getMonth() + iObj.getScheduledDate().getDayOfMonth() + iObj.getInspectionDate().getYear();
+                                    var inspKey = inspDate + "-" + inspType + "-" + iObj.getInspectionStatus() + "-" + iNumber;
+                                    if (!isSupervisor(currentUserID) && !containsAcceptedAdHocTaskForDisposition(capId, inspKey)) {
+                                        addAdHocTask("ADHOC_WORKFLOW", "Supervisor Review Inspection", inspKey, assignTo);
+                                        logDebug("Added Supervisor Review Inspection Task");
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        logDebug("**ERROR retrieving inspection " + iNumber + " : " + iObjResult.getErrorMessage());
+                    }
+                } else {
+                    logDebug("Incorrect parameters");
+                }
+            } else {
+                logDebug("Unknown adhoc task type");
             }
         }
-        else {
-            logDebug("Unknown adhoc task type");
-        }
     }
+
+
 }
