32e8dd5252394ba5ddec943c7fa02bf5a92fd2e0
Richard Voller <rvoller@hhcorp.org>
2020-03-02 14:33:53 -0500

    Update ASA;ENVHEALTH!FOOD!~!~.js

--- Scripts/Event/ASA;ENVHEALTH!FOOD!~!~.js
+++ Scripts/Event/ASA;ENVHEALTH!FOOD!~!~.js
@@ -1,9 +1,11 @@
 showDebug = true;
 showMessage = true;
 var areaInspector = '';
+var zone = getGISInfo("MCPHD", "FoodsDistrict", "district");
 
 //CPS EHS
 if (matches(appTypeArray[3],'Application')) {
 	areaInspector = hhcgetUserByDiscipline('FoodsApplications');
 	assignTask('Application Intake',areaInspector);
+	editAppSpecific('District',zone);
 	}
