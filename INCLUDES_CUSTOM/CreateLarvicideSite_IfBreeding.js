5c185cbb2e5a3b82c87c4ea8982c7e74ddc1d096
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-03-13 16:04:05 -0400

    Update CreateLarvicideSite_IfBreeding.js

--- INCLUDES_CUSTOM/CreateLarvicideSite_IfBreeding.js
+++ INCLUDES_CUSTOM/CreateLarvicideSite_IfBreeding.js
@@ -7,12 +7,36 @@ function CreateLarvicideSite_IfBreeding(capId){
 		asiLabel = "Is Site Breeding";
 		var myResult = getGuidesheetASIValue(inspId,gName,gItem,asiGroup,asiSubGroup, asiLabel);
 			if(myResult=="Yes"){
-			//Create the Larvicide Site Case
-			newChildID = createChild('EnvHealth','VC','LarvicideSite','NA','');
-			// Add Case and Data Fields Info
-			copyAppSpecific(newChildID);
-						}
-}
+                //Create the Larvicide Site Case
+                var newAppId = createChild('EnvHealth','VC','LarvicideSite','NA','');
+                // Add Case and Data Fields Info
+                copyAppSpecific(newAppId);
+                var aZone = getVectorZone(capId);
+
+                var oldCapId = capId;
+                capId = newAppId;
+
+                logDebug(capId.getCustomID());
+
+                copyParcelGisObjects4XAPO();
+                var techByZone = lookup("GIS - Larvicide Techs",aZone); 
+
+                var inspTime = null;
+                var inspComm = "Scheduled via Script";
+                var inspectorObj = null;
+                var DateToSched = nextWorkDay(dateAdd(null,14));
+
+                var inspRes = aa.person.getUser(techByZone);
+                if (inspRes.getSuccess()){
+                    inspectorObj = inspRes.getOutput();
+                }
+            
+                aa.inspection.scheduleInspection(newAppId, inspectorObj, aa.date.parseDate(DateToSched), inspTime, "Larvicide", inspComm)
+                assignCap(techByZone, newAppId);
+
+                capId = oldCapId;
+			}
+    }
 	catch(err)
 	{
 		logDebug("A JavaScript Error occurred: CreateLarvicideSite_IfBreeding:  " + err.message);
