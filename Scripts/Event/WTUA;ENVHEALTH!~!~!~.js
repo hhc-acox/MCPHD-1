171c87aa590c1c2cca7206b6b5b7be09e7010078
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-03-13 13:51:24 -0400

    Update WTUA;ENVHEALTH!~!~!~.js

--- Scripts/Event/WTUA;ENVHEALTH!~!~!~.js
+++ Scripts/Event/WTUA;ENVHEALTH!~!~!~.js
@@ -1,5 +1,74 @@
-addSupervisorReview(capId, "WORKFLOW", null, wfTask, wfStatus);
+showDebug = true;
+showMessage = true;
 
-if(wfTask.indexOf('Supervisor Review') > -1 && wfStatus == 'Accepted') {
-    continueSupervisorReview(wfTask, wfNote);
+//assess fees
+try{
+	//see if any records are set up--module can be specific or "ALL", look for both
+	var sepScriptConfig = aa.cap.getCapIDsByAppSpecificInfoField("Module Name", appTypeArray[0]);
+	if(sepScriptConfig.getSuccess()){
+		var sepScriptConfigArr = sepScriptConfig.getOutput();
+		if(sepScriptConfigArr.length<1){
+			var sepScriptConfig = aa.cap.getCapIDsByAppSpecificInfoField("Module Name", "ALL");
+			if(sepScriptConfig.getSuccess()){
+				var sepScriptConfigArr = sepScriptConfig.getOutput();
+			}
+		}
+		aa.print("sepFees Script Config: " + sepScriptConfig.length);
+		if(sepScriptConfigArr.length>0){
+						for(sep in sepScriptConfigArr){
+				var cfgCapId = sepScriptConfigArr[sep].getCapID();
+				var sepFees = loadASITable("FEES - WORKFLOW",cfgCapId);
+				aa.print("sepFees: " + sepFees.length);
+				if(sepFees.length>0){
+					sepUpdateFeesWkfl(sepFees);
+				}
+			}
+		}
+	}
+}catch(err){
+	logDebug("A JavaScript Error occurred: WTUA:*/*/*/*: Assess Fees: " + err.message);
+	logDebug(err.stack)
 }
+try{
+	HHC_doWorkflowActions();
+}catch(err){
+	logDebug("A JavaScript Error occurred: WTUA:*/*/*/*: Workflow Actions: " + err.message);
+	logDebug(err.stack)
+}
+
+//issue license
+try{
+	sepIssueLicenseWorkflow();
+}catch(err){
+	logDebug("A JavaScript Error occurred: WTUA:*/*/*/*: Issue license: " + err.message);
+	logDebug(err.stack)
+}
+//send notifications--should always be the last script, especially if the notification is based on fees or other logic.
+try{
+	//see if any records are set up--module can be specific or "ALL", look for both
+	var sepScriptConfig = aa.cap.getCapIDsByAppSpecificInfoField("Module Name", appTypeArray[0]);
+	if(sepScriptConfig.getSuccess()){
+		var sepScriptConfigArr = sepScriptConfig.getOutput();
+		if(sepScriptConfigArr.length<1){
+			var sepScriptConfig = aa.cap.getCapIDsByAppSpecificInfoField("Module Name", "ALL");
+			if(sepScriptConfig.getSuccess()){
+				var sepScriptConfigArr = sepScriptConfig.getOutput();
+			}
+		}
+		if(sepScriptConfigArr.length>0){
+			for(sep in sepScriptConfigArr){
+				var cfgCapId = sepScriptConfigArr[sep].getCapID();
+				var sepNotifList = loadASITable("NOTIFICATIONS - WORKFLOW",cfgCapId);
+				for(row in sepNotifList){
+					if(sepNotifList[row]["Active"]=="Yes"){
+						sepEmailNotifContactWkfl(sepNotifList[row]["Record Type"], sepNotifList[row]["Contact Type"], sepNotifList[row]["Respect Preferred Channel"], sepNotifList[row]["Notification Name"], sepNotifList[row]["Report Name"], sepNotifList[row]["Task Name"], sepNotifList[row]["Task Status"], getAppSpecific("Agency From Email",cfgCapId), sepNotifList[row]["Additional Query"]);
+					}
+				}
+			}
+		}
+	}
+}catch(err){
+	logDebug("A JavaScript Error occurred: WTUA:*/*/*/*: Send Notifications: " + err.message);
+	logDebug(err.stack)
+}
+
