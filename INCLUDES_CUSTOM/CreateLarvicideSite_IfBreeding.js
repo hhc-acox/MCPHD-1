7108c8c0033f3ff6222f9aad74c1be9ee718cec4
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-03-16 15:18:34 -0400

    Update CreateLarvicideSite_IfBreeding.js

--- INCLUDES_CUSTOM/CreateLarvicideSite_IfBreeding.js
+++ INCLUDES_CUSTOM/CreateLarvicideSite_IfBreeding.js
@@ -8,7 +8,7 @@ function CreateLarvicideSite_IfBreeding(capId){
 		var myResult = getGuidesheetASIValue(inspId,gName,gItem,asiGroup,asiSubGroup, asiLabel);
 			if(myResult=="Yes"){
                 //Create the Larvicide Site Case
-                var newAppId = createChild('EnvHealth','VC','LarvicideSite','NA','');
+                var newAppId = createParent('EnvHealth','VC','LarvicideSite','NA','');
                 // Add Case and Data Fields Info
                 copyAppSpecific(newAppId);
                 var aZone = getVectorZone(capId);
@@ -33,6 +33,34 @@ function CreateLarvicideSite_IfBreeding(capId){
             
                 aa.inspection.scheduleInspection(newAppId, inspectorObj, aa.date.parseDate(DateToSched), inspTime, "Larvicide", inspComm)
                 assignCap(techByZone, newAppId);
+                
+                //SET RECORD ID BASED ON ZONE
+                var zone4cap = "00"; //default zone
+                var tmpZone = "";
+                if (AInfo["Zone"] != null && AInfo["Zone"] != "") {
+                    tmpZone = String(AInfo["Zone"]).replace(/\D/g, '').trim();
+                    logDebug("Using ASI Zone Info = " + tmpZone);
+                }
+                else {
+                    //if no ASI try GIS
+                    tmpZone = getGISInfo("MCPHD", "VectorZones", "vectorzone");
+                    logDebug("Using GIS Zone Info = " + tmpZone);
+                    tmpZone = String(tmpZone).replace(/\D/g, '').trim();
+                }
+                if (tmpZone != "") {
+                    if (!isNaN(tmpZone)) {
+                        tmpZone = String(parseInt(tmpZone, 10)); //strip the zeros
+                    }
+                    zone4cap = tmpZone;
+                    zone4cap = zeroPad(zone4cap, 2);
+                }
+
+                var capStringArr = String(capIDString).split("-");
+                if (capStringArr.length == 2) {
+                    var newId = capStringArr[0] + "-" + zone4cap + "-" + capStringArr[1];
+                    logDebug("New ID " + newId);
+                    aa.cap.updateCapAltID(capId, newId);
+                }
 
                 capId = oldCapId;
 			}
