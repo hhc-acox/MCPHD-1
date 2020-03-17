66e02be499e7c1a529f961a4b60a79631b00f9ab
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-03-17 15:18:44 -0400

    Update CreateLarvicideSite_IfBreeding.js

--- INCLUDES_CUSTOM/CreateLarvicideSite_IfBreeding.js
+++ INCLUDES_CUSTOM/CreateLarvicideSite_IfBreeding.js
@@ -52,12 +52,36 @@ function CreateLarvicideSite_IfBreeding(capId){
                         tmpZone = String(parseInt(tmpZone, 10)); //strip the zeros
                     }
                     zone4cap = tmpZone;
-                    zone4cap = zeroPad(zone4cap, 2);
+                    //zone4cap = zeroPad(zone4cap, 2);
                 }
 
+                var vc_conn = new db();
+                var vc_sql = "SELECT b.B1_ALT_ID " +
+                    "FROM B1PERMIT b " + 
+                    "WHERE b.B1_ALT_ID like 'LVC-" + zone4cap + "-%' " + 
+                    "ORDER BY b.B1_ALT_ID DESC";
+                var ds = vc_conn.dbDataSet(vc_sql, 25);
+                var dsCapIdString = "0-0-0";
+                
+                if (ds[0]) {
+                    dsCapIdString = ds[0]["B1_ALT_ID"];
+                } 
                 var capStringArr = String(capIDString).split("-");
+
+                var dsNewId = capStringArr[1];
+
+                if (dsCapIdString) {
+                    logDebug('Using db-based naming: ' + dsCapIdString);
+                    var newCapStringArr = String(dsCapIdString).split("-");
+                    logDebug('Highest Seq: ' + parseInt(newCapStringArr[2]));
+                    dsNewId = parseInt(newCapStringArr[2]) + 1;
+
+                    if (dsNewId < 9) {
+                        dsNewId = '0' + dsNewId;
+                    }
+                }
                 if (capStringArr.length == 2) {
-                    var newId = capStringArr[0] + "-" + zone4cap + "-" + capStringArr[1];
+                    var newId = capStringArr[0] + "-" + zone4cap + "-" + dsNewId;
                     logDebug("New ID " + newId);
                     aa.cap.updateCapAltID(capId, newId);
                 }
