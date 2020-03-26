f0052099d72ea02eaa14b95b62885c0050abd871
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-03-26 10:28:28 -0400

    Update IRSA;ENVHEALTH!~!~!~.js

--- Scripts/Event/IRSA;ENVHEALTH!~!~!~.js
+++ Scripts/Event/IRSA;ENVHEALTH!~!~!~.js
@@ -1,7 +1,7 @@
 showDebug = true; 
 showMessage = true; 
 
-if (appTypeString.indexOf('WQ') > -1 || appTypeString.indexOf('Radon')) {
+if (appTypeString.indexOf('WQ') > -1 || appTypeString.indexOf('Radon') > -1) {
     addSupervisorReview(capId, "INSPECTION", inspId, null, null);
 }
 
