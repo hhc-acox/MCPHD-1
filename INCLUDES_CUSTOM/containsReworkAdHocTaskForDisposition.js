9a83c1bc180fa006df3801ec711346e49a1e65b2
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-03-30 13:21:25 -0400

    Update containsReworkAdHocTaskForDisposition.js

--- INCLUDES_CUSTOM/containsReworkAdHocTaskForDisposition.js
+++ INCLUDES_CUSTOM/containsReworkAdHocTaskForDisposition.js
@@ -2,7 +2,7 @@ function containsReworkAdHocTaskForDisposition(capId, dispositionNote) {
 	var tasks = aa.workflow.getTasks(capId).getOutput();
 	
 	for(e in tasks) {
-		if(tasks[e].getTaskDescription().indexOf("Supervisor Review") > -1 && tasks[e].getDispositionNote().equals(dispositionNote) && tasks[e].getDisposition().equals("Accepted")) {
+		if(tasks[e].getTaskDescription().indexOf("Supervisor Review") > -1 && tasks[e].getDispositionNote().equals(dispositionNote) && tasks[e].getDisposition().equals("Rework")) {
 			return true;
 		}
 	}
