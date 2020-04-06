d156b14d8a24243c5b7198f8e210a69f4dd983c2
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-04-06 14:11:14 -0400

    Update IRSA;ENVHEALTH!VC!LARVICIDESITE!~.js

--- Scripts/Event/IRSA;ENVHEALTH!VC!LARVICIDESITE!~.js
+++ Scripts/Event/IRSA;ENVHEALTH!VC!LARVICIDESITE!~.js
@@ -1,44 +1,51 @@
-showDebug = true; 
-showMessage = true; 
+showDebug = true;
+showMessage = true;
 var resultDate = sysDate;
 var resultComment = 'Updated by Script';
-var inspStatus = 'Closed';
-if (matches(inspResult,'Technician Complete')) {
-	//assign to Mosquito Control Team Leader
-	var userID = hhcgetUserByDiscipline('VCMosquito');
 
-        if (isSupervisor(currentUserID)) {
-            logDebug('User is supervisor');
-            userID = currentUserID;
+gName = "VC_LARVICIDE";
+gItem = "SITE INFORMATION";
+asiGroup = "VC_LVCCKLST";
+asiSubGroup = "LARVICIDE";
+asiLabel = "Is Site Breeding";
+asiLabelSample = "Sample Collected?";
+var myResult = getGuidesheetASIValue(inspId, gName, gItem, asiGroup, asiSubGroup, asiLabel);
+var sampleResult = getGuidesheetASIValue(inspId, gName, gItem, asiGroup, asiSubGroup, asiLabelSample);
+copyParcelGisObjects4XAPO();
+var aZone = getAppSpecific('Zone');
+if (aZone.indexOf('Vector') < 0) {
+    aZone = 'Vector Zone ' + zeroPad(aZone, 2);
+}
+
+if (matches(inspResult, 'Technician Complete')) {
+    //assign to Mosquito Control Team Leader
+    var userID = hhcgetUserByDiscipline('VCMosquito');
+
+    //if (isSupervisor(currentUserID)) {
+        if (sampleResult == "Y" || sampleResult == "Yes") {
+            userID = hhcgetUserByDiscipline('VCBiology2'); // bio 2 if zone 9 or above
+    
+            var vcZone = aZone;
+            logDebug('Zone: ' + vcZone);
+            if (vcZone) {
+                vcZone = vcZone.slice(-2);
+                logDebug('Zone Substr: ' + vcZone);
+                vcZone = parseInt(vcZone);
+                logDebug('Zone Parsed: ' + vcZone);
+                if (vcZone > 8) {
+                    userID = hhcgetUserByDiscipline('VCBiology'); // bio 1 if zone 1-8
+                }
+            }
+            aa.inspection.resultInspection(capId, inspId, 'Waiting on Lab', resultDate, resultComment, currentUserID);
+            assignCap(userID);
         }
-	assignInspection(inspId, userID);
+    //}
+    assignInspection(inspId, userID);
 }
-if (matches(inspResult,'Supervisor Reviewed')) {
-	//assign to Mosquito Control Biology
-	var userID = hhcgetUserByDiscipline('VCBiology');
-	assignInspection(inspId, userID);
-}
-if (matches(inspResult,'Lab Complete')) {
-	gName = "VC_LARVICIDE";
-	gItem = "SITE INFORMATION";
-	asiGroup = "VC_LVCCKLST";
-	asiSubGroup = "LARVICIDE";
-    asiLabel = "Is Site Breeding";
-    asiLabelSample = "Sample Collected?";
-    var myResult = getGuidesheetASIValue(inspId,gName,gItem,asiGroup,asiSubGroup, asiLabel);
-    var sampleResult = getGuidesheetASIValue(inspId,gName,gItem,asiGroup,asiSubGroup, asiLabelSample);
-	copyParcelGisObjects4XAPO(); 
-    var aZone = getAppSpecific('Zone');
-    if (aZone.indexOf('Vector') < 0) {
-        aZone = 'Vector Zone ' + zeroPad(aZone, 2);
-    }
-	var techByZone = lookup("GIS - Larvicide Techs",aZone); 
-	if(myResult=="Y" || myResult=="Yes")	{
-        scheduleInspectDate("Larvicide",nextWorkDay(dateAdd(inspResultDate,13)),techByZone);
-        //aa.inspection.resultInspection(capId, inspId, inspStatus, resultDate, resultComment, currentUserID);
-        updateAppStatus("Active");
-    }
-    if(sampleResult=="Y" || sampleResult=="Yes"){
+
+if (matches(inspResult, 'Supervisor Reviewed')) {
+    //assign to Mosquito Control Biology
+    if (sampleResult == "Y" || sampleResult == "Yes") {
         var userID = hhcgetUserByDiscipline('VCBiology2'); // bio 2 if zone 9 or above
 
         var vcZone = aZone;
@@ -52,11 +59,38 @@ if (matches(inspResult,'Lab Complete')) {
                 userID = hhcgetUserByDiscipline('VCBiology'); // bio 1 if zone 1-8
             }
         }
-
-	    assignCap(userID);
+        aa.inspection.resultInspection(capId, inspId, 'Waiting on Lab', resultDate, resultComment, currentUserID);
+        assignCap(userID);
+        assignInspection(inspId, userID);
     }
 }
-if (matches(inspResult,'Unable to Inspect','No Access')) {
-	//aa.inspection.resultInspection(capId, inspId, inspStatus, resultDate, resultComment, currentUserID);
-	updateAppStatus("Active");
+
+if (matches(inspResult, 'Lab Complete')) {
+    
+    var techByZone = lookup("GIS - Larvicide Techs", aZone);
+    if (myResult == "Y" || myResult == "Yes") {
+        scheduleInspectDate("Larvicide", nextWorkDay(dateAdd(inspResultDate, 13)), techByZone);
+        //aa.inspection.resultInspection(capId, inspId, inspStatus, resultDate, resultComment, currentUserID);
+        updateAppStatus("Active");
+    }
+    if (sampleResult == "Y" || sampleResult == "Yes") {
+        var userID = hhcgetUserByDiscipline('VCBiology2'); // bio 2 if zone 9 or above
+
+        var vcZone = aZone;
+        logDebug('Zone: ' + vcZone);
+        if (vcZone) {
+            vcZone = vcZone.slice(-2);
+            logDebug('Zone Substr: ' + vcZone);
+            vcZone = parseInt(vcZone);
+            logDebug('Zone Parsed: ' + vcZone);
+            if (vcZone > 8) {
+                userID = hhcgetUserByDiscipline('VCBiology'); // bio 1 if zone 1-8
+            }
+        }
+        //assignCap(userID);
+    }
+}
+if (matches(inspResult, 'Unable to Inspect', 'No Access')) {
+    //aa.inspection.resultInspection(capId, inspId, inspStatus, resultDate, resultComment, currentUserID);
+    updateAppStatus("Active");
 }
