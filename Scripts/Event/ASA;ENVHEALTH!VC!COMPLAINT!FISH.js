d71c33a7ef9bfbb32ac458c2605761865b24e33c
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-04-06 16:20:43 -0400

    Update ASA;ENVHEALTH!VC!COMPLAINT!FISH.js

--- Scripts/Event/ASA;ENVHEALTH!VC!COMPLAINT!FISH.js
+++ Scripts/Event/ASA;ENVHEALTH!VC!COMPLAINT!FISH.js
@@ -1,7 +1,7 @@
 //ASA;ENVHEALTH!VC!COMPLAINT!FISH
 var aZone = getVectorZone(capId);
 var userID = hhcgetUserByDiscipline('VCFish');
-scheduleInspectDate("Fish",nextWorkDay(dateAdd(null,0,"Scheduled by script")),userID);
+scheduleInspectDate("Fish Stocking",nextWorkDay(dateAdd(null,0,"Scheduled by script")),userID);
 editAppSpecific("Zone",aZone);
 assignCap(userID);
 updateAppStatus("Assigned");
