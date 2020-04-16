429d1bcd4b9eafb4c63b300df0ed560efa3ac630
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-04-16 09:55:34 -0400

    Create IRMA;ENVHEALTH!VC!COMPLAINT!LARVICIDE.js

---
+++ Scripts/Event/IRMA;ENVHEALTH!VC!COMPLAINT!LARVICIDE.js
@@ -0,0 +1,12 @@
+// Enter your script here...
+
+if (matches(inspResult,'Tech Complete - New Site','Tech Complete - Existing Site')) {
+	//assign to Mosquito Control Biology
+	var userID = hhcgetUserByDiscipline('VCBiology');
+	assignInspection(inspId, userID);
+	
+	//create site if new
+	if (matches(inspResult, 'Tech Complete - New Site')) {
+		CreateLarvicideSite_IfBreeding(capId);	
+	}
+}
