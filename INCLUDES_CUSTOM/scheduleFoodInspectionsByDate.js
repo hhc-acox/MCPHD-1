38be1b3919320f827d7dc37ae02888ee08d27207
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-04-24 14:04:38 -0400

    Update scheduleFoodInspectionsByDate.js

--- INCLUDES_CUSTOM/scheduleFoodInspectionsByDate.js
+++ INCLUDES_CUSTOM/scheduleFoodInspectionsByDate.js
@@ -2,6 +2,7 @@ function scheduleFoodInspectionsByDate(iType, dateToSched, inspectorId, tCapId) 
     var inspRes = scheduleInspectDateAndReturnId(iType, dateToSched, inspectorId);
 
     if (inspRes) {
+        doGuidesheetAutomation(inspRes, iType);
         doFoodsChecklists(inspRes, tCapId);
     }
 }
