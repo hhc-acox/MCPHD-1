229895f821ab9baebb000693d0686edfa8949f27
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-04-28 10:45:00 -0400

    Update ASA;ENVHEALTH!FOOD!~!~.js

--- Scripts/Event/ASA;ENVHEALTH!FOOD!~!~.js
+++ Scripts/Event/ASA;ENVHEALTH!FOOD!~!~.js
@@ -1,4 +1,4 @@
-if (matches(appTypeArray[3],'Application') || matches(appTypeArray[2],'TempEvent')) {
+if (matches(appTypeArray[3],'Application') && matches(appTypeArray[2],'TempEvent')) {
 	areaInspector = getAssignedToRecord();
 	assignTask('Application Intake',areaInspector);
 }
