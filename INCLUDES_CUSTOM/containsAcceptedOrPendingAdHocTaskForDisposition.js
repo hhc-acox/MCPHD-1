0278f58527e0f9b0435bc23c9e3126b1f4f19fab
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-04-30 14:11:11 -0400

    Update containsAcceptedOrPendingAdHocTaskForDisposition.js

--- INCLUDES_CUSTOM/containsAcceptedOrPendingAdHocTaskForDisposition.js
+++ INCLUDES_CUSTOM/containsAcceptedOrPendingAdHocTaskForDisposition.js
@@ -2,7 +2,7 @@ function containsAcceptedOrPendingAdHocTaskForDisposition(capId, dispositionNote
 	var tasks = aa.workflow.getTasks(capId).getOutput();
 	
 	for(e in tasks) {
-		if(tasks[e].getTaskDescription().indexOf("Supervisor Review") > -1 && tasks[e].getDispositionNote() == dispositionNote && (tasks[e].getDisposition() == "Accepted" || tasks[e].getDisposition() == "In Progress")) {
+		if(tasks[e].getTaskDescription().indexOf("Supervisor Review") > -1 && tasks[e].getDispositionNote() == dispositionNote && (tasks[e].getDisposition() == "Accepted" || tasks[e].getDisposition() == "In Progress" || tasks[e].getDisposition() == null)) {
 			return true;
 		}
 	}
