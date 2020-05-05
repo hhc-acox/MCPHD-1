1e12d3f304ce4d370cdbb3f123556e58cf51712c
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-05-05 11:18:56 -0400

    Update IRSA;ENVHEALTH!~!~!~.js

--- Scripts/Event/IRSA;ENVHEALTH!~!~!~.js
+++ Scripts/Event/IRSA;ENVHEALTH!~!~!~.js
@@ -2,7 +2,9 @@ showDebug = true;
 showMessage = true; 
 
 if ((appTypeString.indexOf('WQ') > -1 || appTypeString.indexOf('Radon') > -1) && appTypeString.indexOf('Watershed') < 0) {
-    addSupervisorReview(capId, "INSPECTION", inspId, null, null);
+    if (inspResult && inspResult != '') {
+        addSupervisorReview(capId, "INSPECTION", inspId, null, null);
+    }
 }
 
 addCurrentViolations();
