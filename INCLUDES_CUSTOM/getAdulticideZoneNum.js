1829f56059a7f88b528f31e53c87daa9b511b8e2
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-05-12 16:24:20 -0400

    Update getAdulticideZoneNum.js

--- INCLUDES_CUSTOM/getAdulticideZoneNum.js
+++ INCLUDES_CUSTOM/getAdulticideZoneNum.js
@@ -9,6 +9,12 @@ function getAdulticideZoneNum(capId){
 		z4 = getGISInfo("MCPHD","fogging4","foggingname");
 		z5 = getGISInfo("MCPHD","fogging5","foggingname");
 		z6 = getGISInfo("MCPHD","fogging6","foggingname");
+		z6 = getGISInfo("MCPHD","fogging6","foggingname");
+		z7 = getGISInfo("MCPHD","fogging7","foggingname");
+		z8 = getGISInfo("MCPHD","fogging8","foggingname");
+		z9 = getGISInfo("MCPHD","fogging9","foggingname");
+		z10 = getGISInfo("MCPHD","fogging10","foggingname");
+		
 				if (z1 == 1){
 					aZone = z1;	
 		}
@@ -26,6 +32,18 @@ function getAdulticideZoneNum(capId){
 		}
 				if (z6 == 6){
 					aZone = z6;
+		}
+		if (z7 == 7){
+					aZone = z7;
+		}
+		if (z8 == 8){
+					aZone = z8;
+		}
+		if (z9 == 9){
+					aZone = z9;
+		}
+		if (z10 == 10){
+					aZone = z10;
 		}
 			return aZone;
 		}
