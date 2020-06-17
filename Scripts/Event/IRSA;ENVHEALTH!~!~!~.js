b02fac16896e9afa07a3756430a0673a6ed8c253
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-06-17 08:11:13 -0400

    Update IRSA;ENVHEALTH!~!~!~.js

--- Scripts/Event/IRSA;ENVHEALTH!~!~!~.js
+++ Scripts/Event/IRSA;ENVHEALTH!~!~!~.js
@@ -2,7 +2,7 @@ showDebug = true;
 showMessage = true; 
 
 if ((appTypeString.indexOf('WQ') > -1 || appTypeString.indexOf('Radon') > -1) && appTypeString.indexOf('Watershed') < 0) {
-    if (inspResult && inspResult != '') {
+    if (inspResult && inspResult != '' && inspResult != 'Scheduled') {
         addSupervisorReview(capId, "INSPECTION", inspId, null, null);
     }
 }
