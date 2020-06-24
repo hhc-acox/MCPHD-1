0cdf1a811cc704fc2fcea23d7afc09281f1b80b1
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-06-24 14:09:54 -0400

    Update validateForCourt.js

--- INCLUDES_CUSTOM/validateForCourt.js
+++ INCLUDES_CUSTOM/validateForCourt.js
@@ -1,31 +1,31 @@
 function validateForCourt() {
-	showMessage = true;
-	itemCap = capId
-	if (arguments.length > 0)	itemCap = arguments[0]
-	localCancel = false;
-	errMess = "";
+    showMessage = true;
+    itemCap = capId
+    if (arguments.length > 0) itemCap = arguments[0]
+    localCancel = false;
+    errMess = "";
 
-	cContactResult = aa.people.getCapContactByCapID(capId);
-	if (cContactResult.getSuccess()) {
-		conArray = cContactResult.getOutput();
-		if (conArray && conArray.length > 0) {
-			useThisContact = null;
-			for (var ci in conArray) {
-				thisContact = conArray[ci];
-				thisPeop = thisContact.getPeople();
-				if (thisPeop.getFlag() == "Y") 
-					useThisContact = thisContact;
-				break;
-			}
-			if (useThisContact == null) {
-				useThisContact = conArray[0];
-			}
-			conPeop = useThisContact.getPeople();
+    cContactResult = aa.people.getCapContactByCapID(capId);
+    if (cContactResult.getSuccess()) {
+        conArray = cContactResult.getOutput();
+        if (conArray && conArray.length > 0) {
+            useThisContact = null;
+            for (var ci in conArray) {
+                thisContact = conArray[ci];
+                thisPeop = thisContact.getPeople();
+                if (thisPeop.getFlag() == "Y")
+                    useThisContact = thisContact;
+                break;
+            }
+            if (useThisContact == null) {
+                useThisContact = conArray[0];
+            }
+            conPeop = useThisContact.getPeople();
             // check fields for individual
             var individualValid = true;
             var businessValid = true;
 
-            if ((!conPeop.getFirstName() || conPeop.getFirstName() == "") || (!conPeop.getLastName() || conPeop.getLastName() == "" )) {
+            if ((!conPeop.getFirstName() || conPeop.getFirstName() == "") || (!conPeop.getLastName() || conPeop.getLastName() == "")) {
                 logDebug('Individual Invalid');
                 individualValid = false;
                 //localCancel = true;
@@ -43,49 +43,65 @@ function validateForCourt() {
                 errMess += "Individual contact must have first and last name or a business name";
                 localCancel = true;
             }
-			// check fields for both types
-			cAddr = conPeop.getCompactAddress();
-			if (!cAddr.getCity() || cAddr.getCity() == "" ) {
-				errMess += "Contact address must have a city"; localCancel = true; }
-			if (!cAddr.getState() || cAddr.getState() == "" ) {
-				errMess += "Contact address must have a state"; localCancel = true; }
-			if (!cAddr.getZip() || cAddr.getZip() == "" ) {
-				errMess += "Contact address must have a zip"; localCancel = true; }
-			if (!cAddr.getAddressLine1() || cAddr.getAddressLine1() == "" ) {
-				errMess += "Contact address must have an address"; localCancel = true; }
-		}
-		else {
-			localCancel = true;
-			errMess += "Missing court info: At least one contact is required.";
-		}
-	}
-	else {
-		logDebug("Error retrieving contacts " + cContactResult.getErrorMessage());
-		errMess += "Missing court info: At least one contact is required.";
-	}
+            // check fields for both types
+            cAddr = conPeop.getCompactAddress();
+            
+            if (!cAddr.getCity() || cAddr.getCity() == "") {
+                errMess += "Contact address must have a city";
+                localCancel = true;
+            }
+            if (!cAddr.getState() || cAddr.getState() == "") {
+                errMess += "Contact address must have a state";
+                localCancel = true;
+            }
+            if (!cAddr.getZip() || cAddr.getZip() == "") {
+                errMess += "Contact address must have a zip";
+                localCancel = true;
+            }
+            if (!cAddr.getAddressLine1() || cAddr.getAddressLine1() == "") {
+                errMess += "Contact address must have an address";
+                localCancel = true;
+            } else {
+                var strAddress = cAddr.getAddressLine1();
+                //comment('Address Below:');
+                //comment(strAddress);
+                //comment(strAddress.length);
+                if (strAddress.length() > 64) {
+                    errMess += "Address cannot exceed 64 characters";
+                    localCancel = true;
+                }
+            }
+        } else {
+            localCancel = true;
+            errMess += "Missing court info: At least one contact is required.";
+        }
+    } else {
+        logDebug("Error retrieving contacts " + cContactResult.getErrorMessage());
+        errMess += "Missing court info: At least one contact is required.";
+    }
 
     if (typeof(DOCUMENTS) != "object") {
-    	localCancel = true;
-    	errMess += "Missing court info: At least one entry in the DOCUMENTS list is required.";  	
+        localCancel = true;
+        errMess += "Missing court info: At least one entry in the DOCUMENTS list is required.";
     }
     if (typeof(DOCUMENTS) == "object" && DOCUMENTS.length == 0) {
-    	localCancel = true;
-    	errMess += "Missing court info: At least one entry in the DOCUMENTS list is required.";  	
+        localCancel = true;
+        errMess += "Missing court info: At least one entry in the DOCUMENTS list is required.";
     }
-    
+
     if (typeof(OFFENSECODES) != "object") {
-    	localCancel = true;
-    	errMess += "Missing court info: At least one entry in the OFFENSE CODES list is required.";  	
+        localCancel = true;
+        errMess += "Missing court info: At least one entry in the OFFENSE CODES list is required.";
     }
     if (typeof(OFFENSECODES) == "object" && OFFENSECODES.length == 0) {
-    	localCancel = true;
-    	errMess += "Missing court info: At least one entry in the OFFENSE CODES list is required.";  	
+        localCancel = true;
+        errMess += "Missing court info: At least one entry in the OFFENSE CODES list is required.";
     }
-    
-  
 
-	if (localCancel) {
-		cancel = true;
-		comment(errMess);
-	}
+
+
+    if (localCancel) {
+        cancel = true;
+        comment(errMess);
+    }
 }
