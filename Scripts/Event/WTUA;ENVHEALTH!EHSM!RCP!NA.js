e3c82495a00455ae95597efab06609ada145e084
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-07-01 14:30:01 -0400

    Update WTUA;ENVHEALTH!EHSM!RCP!NA.js

--- Scripts/Event/WTUA;ENVHEALTH!EHSM!RCP!NA.js
+++ Scripts/Event/WTUA;ENVHEALTH!EHSM!RCP!NA.js
@@ -15,14 +15,39 @@ if (wfTask == 'Evaluation' && wfStatus == 'Court Order Needed') {
 
 try {
     var TRARec = getParent();
+    var rcpId = capId.getCustomID();
 
     logDebug("Parent ID " + TRARec.getCustomID());
     var saveID = capId;
     capId = TRARec;
+    var traId = capId.getCustomID();
 
-    if (((wfTask == "Final Processing" && wfStatus == "Finaled") || (wfTask == "Evaluation" && wfStatus == "No Work Assignment") || (wfTask == 'Billing' && wfStatus == 'Complete Billing Letter') || (wfTask == 'Work Assignment' && wfStatus == 'Cleaned by Other') || (wfTask == 'Work Assignment' && wfStatus == 'Cleaned - No Bill')) && TRARec) {
+    if (((wfTask == "Final Processing" && wfStatus == "Finaled") || (wfTask == "Evaluation" && wfStatus == "No Work Assignment") || (wfTask == 'Billing' && wfStatus == 'Complete Billing Letter') || (wfTask == 'Work Assignment' && wfStatus == 'Cleaned by Other') || (wfTask == 'Work Assignment' && wfStatus == 'Cleaned - No Bill') || (wfTask == 'Work Assignment' && wfStatus == 'Could Not Be Cleaned')) && TRARec) {
         logDebug("Trying to close RCP on TRA");
         closeTask("Request EHSM Clean", "EHSM Cleaned", "Updated by Script", "EHSM Cleaned");
+
+        if (wfTask == 'Work Assignment' && wfStatus == 'Could Not Be Cleaned') {
+            if (currentUserID) {
+                var email = "";
+        
+                if (isSupervisor(currentUserID)) {
+                    logDebug('User is Supervisor');
+                    email = getEmailByUserID(currentUserID);
+                } else {
+                    logDebug('User is not Supervisor');
+                    var supervisorId = HHC_getMyTeamLeadersUserID(currentUserID);
+                    email = getEmailByUserID(supervisorId);
+                }
+        
+                logDebug('Email: ' + email);
+        
+                if (email.indexOf("@") > 0) {
+                    var myemailText = "RCP ("+rcpId+") on TRA ("+traId+") was closed as could not be cleaned."+br+br+"Accela's Automated Email Distribution"
+					
+					aa.sendMail("accela-noreply@marionhealth.org", email, "", "EHSM Clean - Not Cleaned", myemailText);
+                }                        
+            }
+        }
 
         var isPaid = aa.fee.isFullPaid4Renewal(capId).getOutput();
         var ticketActive = false;
