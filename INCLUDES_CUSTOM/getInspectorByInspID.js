862c51c88134f717639545707ffdcf374a0f0697
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-09-02 17:08:54 -0400

    Update getInspectorByInspID.js

--- INCLUDES_CUSTOM/getInspectorByInspID.js
+++ INCLUDES_CUSTOM/getInspectorByInspID.js
@@ -1,13 +1,11 @@
-function getInspectorByInspID(inspNum)
-{
-	var inspResultObj = aa.inspection.getInspection(capId,inspNum);
-	if (inspResultObj.getSuccess())
-    	{
-		var inspObj = inspResultObj.getOutput();
-		
+function getInspectorByInspID(inspNum) {
+    var inspResultObj = aa.inspection.getInspection(capId, inspNum);
+    if (inspResultObj.getSuccess()) {
+        var inspObj = inspResultObj.getOutput();
+
         // have to re-grab the user since the id won't show up in this object.
-        inspUserObj = aa.person.getUser(inspObj.getInspector().getFirstName(),inspObj.getInspector().getMiddleName(),inspObj.getInspector().getLastName()).getOutput();
+        inspUserObj = aa.person.getUser(inspObj.getInspector().getFirstName(), inspObj.getInspector().getMiddleName(), inspObj.getInspector().getLastName()).getOutput();
         return inspUserObj.getUserID();
-    	}
-	return false;
+    }
+    return false;
 }
