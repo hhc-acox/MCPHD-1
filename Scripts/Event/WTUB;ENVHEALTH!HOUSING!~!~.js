cf4a14c43aca1e30a4024156769b0678f7980342
hhc-acox <57953807+hhc-acox@users.noreply.github.com>
2020-09-17 15:40:12 -0400

    Update WTUB;ENVHEALTH!HOUSING!~!~.js

--- Scripts/Event/WTUB;ENVHEALTH!HOUSING!~!~.js
+++ Scripts/Event/WTUB;ENVHEALTH!HOUSING!~!~.js
@@ -1,3 +1,12 @@
 if (matches(wfStatus, "Court", "Court Case", "Refer to Court", "Reinspection", "Complete Reinspection Ltr", "Reinspection Letter", "Complete Reinspection Letter")) {
     validateAdditionalViolations();
 }
+
+if (matches(wfStatus, "Court", "Court Case", "Refer to Court")) {
+    if(!HHC_VALIDATE_OFFENSE_CODES()){
+        cancel = true;
+        showMessage = true;
+        comment("<font color=red><b>Cannot file a court case without offense codes. Please make sure at least one violation matches to an offense code.</b></font>");
+        aa.print("Cannot file a court case with offense codes. Please make sure at least one violation matches to an offense code.");
+    }
+}
